5928afef613e1ff6f93f1b14cf78fa930706cab2
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-01-27 03:20:27 +0900

    [FEAT] #11 모달 컴포넌트 (#21)
    * [STYLE] Toast 컴포넌트 화살표 함수로 변경
    
    * [FEAT] 모달 컴포넌트 기능 개발
    
    * [FEAT] 모달 컴포넌트 타입 작성
    
    * [TEST] 모달 컴포넌트 기능 테스트
    
    * [FEAT] 테일윈드 prettier 설정 추가
    
    * [CHORE] react-dom, prettier 테일윈드 플러그인 패키지 설치

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+'use client';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
 export default function Home() {
-    return <p className="hover:text-main-100">gh2gg2</p>;
+    const [isModal, setIsModal] = useState(true);
+    const Modal = dynamic(() => import('@Component/Common/Modal'), { ssr: false });
+
+    return (
+        <>
+            <h1 className="bg-main-100 hover:text-main-100">hihi</h1>
+            <Modal
+                visible={isModal}
+                onClose={() => setIsModal(false)}
+            >
+                <p className="hover:text-main-100">gh2gg233333333333333333333333333333333</p>
+            </Modal>
+        </>
+    );
 }
