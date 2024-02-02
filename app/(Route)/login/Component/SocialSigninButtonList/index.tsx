88b0faa6651b36d2c751a993f24d600643e6c411
지인혁 <dhgg321@naver.com>
2024-02-02 19:29:19 +0900

    [FEAT] 로그인 버튼 리스트 컴포넌트 개발

---
+++ app/(Route)/login/Component/SocialSigninButtonList/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { IProps } from './type';
+import SocialSigninButtonItem from '../SocialSigninButtonItem';
+
+const SocialSigninButtonList = ({ providers }: IProps) => {
+    const buttonText: Record<string, string> = {
+        Naver: '네이버 로그인',
+        Kakao: '카카오 로그인',
+        Google: '구글 로그인',
+    };
+
+    return (
+        <ul className="flex w-full flex-col gap-[2rem]">
+            {Object.values(providers).map((provider) => {
+                const { id, name } = provider;
+
+                return (
+                    <SocialSigninButtonItem
+                        key={id}
+                        id={id}
+                        name={name}
+                        text={buttonText[name]}
+                    />
+                );
+            })}
+        </ul>
+    );
+};
+
+export default SocialSigninButtonList;
