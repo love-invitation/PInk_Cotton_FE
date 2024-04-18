b1ecbf11b7aee4881a72751f2d113b396f34321e
타래 <85001878+meowTarae@users.noreply.github.com>
2024-04-18 15:37:38 +0900

    🌱feat: layout.tsx에 Tanstack Query Provider 적용

---
+++ src/app/_components/TanstackProvider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { PropsWithChildren, useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const TanstackProvider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
+        },
+      },
+    }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
