c68ccc2a0035aa88b8e9cfaf2eff6ee8f820e329
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-21 19:31:35 +0900

    📝change: Storybook JSDoc 수정

--- src/stories/components/ConfirmModal_Story/ConfirmModal.stories.tsx
+++ src/stories/components/ConfirmModal_Story/ConfirmModal.stories.tsx
@@ -3,17 +3,26 @@ import useModal from '@/hooks/useModal';
 import type { Meta, StoryObj } from '@storybook/react';
 
 /**
- * ## Modal Base Component
+ * ## Confirm Modal Component
  *
  * ### Props
- * - **isShow : **현재 Modal이 보여질지에 대해 boolean 값을 제공합니다.
- * - **onClose ? : **Modal을 닫기위한 상태를 false값으로 변경할수 있는 useModal hook의 close 메서드를 전달합니다.
- * - **children ? : **Modal내부에 표현할 Component 혹은 내용을 전달합니다.
- * - **clickAwayEnable :**Background 클릭, esc키를 통해 Modal 닫기 기능을 사용할지에 대해 전달합니다 - default : false
+ * - **isShow : **useModal Hook에서 isShowModal을 전달 받아 모달의 공개 여부를 지정합니다.
  *
- * 🔥 Modal을 제작하기 위한 layout역할을 하는 Modal Base Component 입니다.
- * <br>
- * **useModal** hook을 같이 사용해야합니다. 내부 showModal, closeModal 메서드를 통해 Modal을 컨트롤할 수 있고 , isShow 값을 Modal에 전달하여 사용할 수 있습니다<div className=""></div>
+ * - **onReject : **Modal이 닫히기 위해 수행될 메서드를 전달받습니다. - Reject Button에도 전달됩니다.
+ * - **onAccept : **Accept Button 클릭시 수행될 메서드를 전달받습니다.
+ *
+ * - **message : **사용자에게 전달할 Confirm 사항 메세지를 전달합니다.
+ * - **warningMessage : **사용자에게 전달할 경고 메세지를 전달합니다.
+ *
+ * - **clickAwayEnable ? :**background click, Escape key를 통한 Modal 제어를 활성화 할지 전달합니다. - default : true
+ *
+ * - **acceptButtonTitle ? : *Accept Button의 타이틀을 변경할 수 있습니다.
+ * - **rejectButtonTitle ? :**Reject Button의 타이틀을 변경할 수 있습니다.
+ *
+ * <hr>
+ *
+ * 기존 Modal Base Component를 이용해 제작한 Confirm Modal 입니다. <br>
+ * 사용 방법은 기존과 동일하게 useModal과 함께 사용해 Modal을 제어할 수 있습니다.
  * */
 const meta = {
   title: 'Components/ConfirmModal',
