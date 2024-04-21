2b23a28d3d5a86aa2209572c93f27f161e86d4e9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 00:57:57 +0900

    🗑️remove: app/page Test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-'use client';
-
-import { AlertModal } from '@/components/client';
-import useModal from '@/hooks/useModal';
-
 const Home = () => {
-  const { showModal, closeModal, isShowModal } = useModal();
-  return (
-    <main className='h-full w-full'>
-      <button
-        type='button'
-        onClick={showModal}
-      >
-        모달 열기
-      </button>
-
-      <AlertModal
-        isShow={isShowModal}
-        onClose={closeModal}
-        message='이건 테스트용 알림 모달 입니다.<br> 이건 테스트용 컨펌 모달 입니다. 이건 테스트용 컨펌 모달 입니다. 이건 테스트용 컨펌 모달 입니다.'
-        subMessage='이건 서브 메세지 이건 서브 메세지 이건 서브 메세지 이건 서브 메세지 이건 서브 메세지 이건 서브 메세지'
-      />
-    </main>
-  );
+  return <main className='h-full w-full'>Pink Cotton</main>;
 };
 
 export default Home;
