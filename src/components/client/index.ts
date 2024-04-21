2c818fde3c22c6424b6f99c659f1b25f959ce401
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 00:03:28 +0900

    🌱feat: create AlertModal component & type

--- src/components/client/index.ts
+++ src/components/client/index.ts
@@ -1,2 +1,3 @@
 export { default as PinkMap } from './PinkMap/PinkMap';
 export { default as ConfirmModal } from './ConfirmModal/ConfirmModal';
+export { default as AlertModal } from './AlertModal/AlertModal';
