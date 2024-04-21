802e72158d06e7ded7548778888664b7c165d892
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 00:53:14 +0900

    🌱feat: add AlertModal message & subMessage

--- src/components/client/AlertModal/AlertModal.tsx
+++ src/components/client/AlertModal/AlertModal.tsx
@@ -1,12 +1,36 @@
 'use client';
 
+import { useEffect } from 'react';
 import Lottie from 'react-lottie-player';
 
 import { Modal } from '../Modal';
 import { AlertModalProps } from './AlertModal.type';
 import AlertAnimationData from './Lottie_Alert_Animation.json';
+import convertSubMessageColor from './utils/convertSubMessageColor';
 
-const AlertModal = ({ isShow, onClose, clickAwayEnable = true }: AlertModalProps) => {
+import { twJoin } from 'tailwind-merge';
+
+const AlertModal = ({
+  isShow,
+  message,
+  subMessage,
+  subMessageType = 'warning',
+  onClose,
+  clickAwayEnable = true,
+}: AlertModalProps) => {
+  useEffect(() => {
+    const messageElement = document.querySelector('#alert_message_container');
+    const subMessageElement = document.querySelector('#alert_sub_message_container');
+
+    if (messageElement) {
+      messageElement.innerHTML = message;
+    }
+
+    if (subMessageElement && subMessage) {
+      subMessageElement.innerHTML = subMessage;
+    }
+  }, [message, subMessage, isShow]);
+
   return (
     <Modal
       isShow={isShow}
@@ -20,6 +44,21 @@ const AlertModal = ({ isShow, onClose, clickAwayEnable = true }: AlertModalProps
           play
           loop={false}
         />
+
+        <p
+          id='alert_message_container'
+          className='px-2 text-center text-size10 font-medium'
+        />
+
+        {subMessage && (
+          <p
+            id='alert_sub_message_container'
+            className={twJoin(
+              'my-2 px-2 text-center text-size4 font-medium',
+              convertSubMessageColor(subMessageType),
+            )}
+          />
+        )}
       </main>
     </Modal>
   );
--- src/components/client/AlertModal/AlertModal.type.ts
+++ src/components/client/AlertModal/AlertModal.type.ts
@@ -1,8 +1,12 @@
+export type AlertSubMessageType = 'warning' | 'basic' | 'safe' | 'danger';
+
 export interface AlertModalProps {
   isShow: boolean;
   onClose: () => void;
   message: string;
   subMessage?: string;
+
+  subMessageType?: AlertSubMessageType;
 
   clickAwayEnable?: boolean;
 }
