6550e4a090c40da278f63abc0dfbc8bf2b6b5295
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-21 17:47:47 +0900

    🌱feat: add Confirm Lottie animation

--- src/components/client/ConfirmModal/ConfirmModal.tsx
+++ src/components/client/ConfirmModal/ConfirmModal.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import Lottie from 'react-lottie-player';
+
 import { Modal } from '../Modal';
 import { ConfirmModalProps } from './ConfirmModal.type';
+import ConfirmAnimationData from './Lottie_Confirm_Animation.json';
 
 const ConfirmModal = ({ isShow, clickAwayEnable = true, onReject }: ConfirmModalProps) => {
   return (
@@ -10,7 +13,14 @@ const ConfirmModal = ({ isShow, clickAwayEnable = true, onReject }: ConfirmModal
       clickAwayEnable={clickAwayEnable}
       onClose={onReject}
     >
-      ConfirmModal
+      <main className='flex w-[30rem] flex-col items-center justify-center'>
+        <Lottie
+          animationData={ConfirmAnimationData}
+          className='h-[24rem] w-[24rem]'
+          play
+          loop={false}
+        />
+      </main>
     </Modal>
   );
 };
