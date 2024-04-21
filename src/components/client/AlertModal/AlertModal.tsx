5bad0207312b68d07bf099d7ad90bf4ac1e900e3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 00:38:30 +0900

    🌱feat: add AlertModal Animation

--- src/components/client/AlertModal/AlertModal.tsx
+++ src/components/client/AlertModal/AlertModal.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import Lottie from 'react-lottie-player';
+
 import { Modal } from '../Modal';
 import { AlertModalProps } from './AlertModal.type';
+import AlertAnimationData from './Lottie_Alert_Animation.json';
 
 const AlertModal = ({ isShow, onClose, clickAwayEnable = true }: AlertModalProps) => {
   return (
@@ -10,7 +13,14 @@ const AlertModal = ({ isShow, onClose, clickAwayEnable = true }: AlertModalProps
       onClose={onClose}
       clickAwayEnable={clickAwayEnable}
     >
-      AlertModal
+      <main className='flex w-[30rem] select-none flex-col items-center justify-center'>
+        <Lottie
+          animationData={AlertAnimationData}
+          className='h-[22rem] w-[22rem]'
+          play
+          loop={false}
+        />
+      </main>
     </Modal>
   );
 };
