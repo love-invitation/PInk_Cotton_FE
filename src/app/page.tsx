471932d8dac7b814a8d5d18dbc67d6e86cdc22e6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-21 17:30:03 +0900

    🌱feat: test Button setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+
+import { ConfirmModal } from '@/components/client';
+import useModal from '@/hooks/useModal';
+
 const Home = () => {
-  return <main className='h-full w-full'>Pink Cotton</main>;
+  const { showModal, closeModal, isShowModal } = useModal();
+  return (
+    <main className='h-full w-full'>
+      <button
+        type='button'
+        onClick={showModal}
+      >
+        모달 열기
+      </button>
+
+      <ConfirmModal
+        isShow={isShowModal}
+        onReject={closeModal}
+        onAccept={() => {}}
+        message='이건 테스트용 컨펌 모달 입니다.'
+      />
+    </main>
+  );
 };
 
 export default Home;
