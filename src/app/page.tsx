ce2daccc0a7ba60be33b9113256f2a19c80d45ce
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 17:28:22 +0900

    🗑️remove: Test Code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-'use client';
-
-import { ConfirmModal } from '@/components/client';
-import useModal from '@/hooks/useModal';
-
 const Home = () => {
-  const { showModal, closeModal, isShowModal } = useModal();
-  return (
-    <main className='h-full w-full'>
-      <button
-        type='button'
-        onClick={showModal}
-      >
-        모달 열기
-      </button>
-
-      <ConfirmModal
-        isShow={isShowModal}
-        onReject={closeModal}
-        onAccept={() => {}}
-        message='이건 테스트용 컨펌 모달 입니다.<br> 이건 테스트용 컨펌 모달 입니다. 이건 테스트용 컨펌 모달 입니다. 이건 테스트용 컨펌 모달 입니다.'
-        warningMessage='이것은 경고 메세지 입니다.이것은 경고 메세지 입니다. 이것은 경고 메세지 입니다. 이것은 경고 메세지 입니다.'
-      />
-    </main>
-  );
+  return <main className='h-full w-full'>PinkCotton</main>;
 };
 
 export default Home;
