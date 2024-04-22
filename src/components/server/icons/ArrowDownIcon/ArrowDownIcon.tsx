d8e1dbc9db7e2d7a10393da572062a1105e0450a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-22 23:16:03 +0900

    🌱feat: ArrowDownIcon

---
+++ src/components/server/icons/ArrowDownIcon/ArrowDownIcon.tsx
@@ -0,0 +1,24 @@
+import { ArrowDownIconProps } from './ArrowDownIcon.type';
+
+const ArrowDownIcon = ({ size = 32, fill = 'black', className = '' }: ArrowDownIconProps) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+    >
+      <path
+        d='M19.92 8.94995L13.4 15.47C12.63 16.24 11.37 16.24 10.6 15.47L4.08002 8.94995'
+        stroke={fill}
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+export default ArrowDownIcon;
