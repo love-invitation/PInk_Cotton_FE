f8312ca3e13fe9be94851e945296f36b82ed406d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-23 23:40:11 +0900

    🌱feat: create Button component

---
+++ src/components/client/Button/Button.tsx
@@ -0,0 +1,17 @@
+import { ButtonProps } from './Button.type';
+
+const Button = ({ children }: ButtonProps) => {
+  return <button type='button'>{children}</button>;
+};
+
+export default Button;
+
+// rounded : 999rem , 1.4rem , 0.8rem , 0.4rem
+
+// border - color :  gray, pink, deep-gray
+
+// shadow - true false
+
+// font : 14 18  20
+
+// hight , width
---
+++ src/components/client/Button/Button.type.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export interface ButtonProps {
+  children: React.ReactNode;
+}
