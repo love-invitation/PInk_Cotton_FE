5771ca43db97424866349563f98d2ef970dcb5e9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-24 00:01:19 +0900

    🌱feat: add Button type

--- src/components/client/Button/Button.type.ts
+++ src/components/client/Button/Button.type.ts
@@ -1,5 +1,15 @@
-import React from 'react';
-
+export type ButtonRadiusType = 'rounded' | '1.4rem' | '0.8rem' | '0.4rem';
+export type ButtonBorderType = 'gray' | 'pink' | 'deep_gray';
+export type ButtonFontSizeType = '2rem' | '1.8rem' | '1.4rem';
 export interface ButtonProps {
   children: React.ReactNode;
+
+  radius?: ButtonRadiusType;
+
+  border?: ButtonBorderType;
+  shadow?: boolean;
+  fontSize?: ButtonFontSizeType;
+
+  height?: number;
+  width?: number;
 }
