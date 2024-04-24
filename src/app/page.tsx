e8dd586b269237050c02090ca2ca0a3a379e484b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-24 19:04:14 +0900

    🗑️remove: page.tsx test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from '@/components/client';
-
 const Home = () => {
-  return (
-    <main className='h-full w-full'>
-      <Button>버튼 테스트 환경</Button>
-    </main>
-  );
+  return <main className='h-full w-full'>Pink Cotton</main>;
 };
 
 export default Home;
