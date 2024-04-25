ec94323f54005def673182046640f8f1da209d82
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-26 02:21:15 +0900

    🌱feat: CheckBox add disabled attributes

--- src/components/client/CheckBox/CheckBox.tsx
+++ src/components/client/CheckBox/CheckBox.tsx
@@ -9,30 +9,39 @@ import { CheckBoxProps } from './CheckBox.type';
 import { motion } from 'framer-motion';
 import { twJoin, twMerge } from 'tailwind-merge';
 
-const CheckBox = ({ checked = false, className = '', size = 20, ...rest }: CheckBoxProps) => {
+const CheckBox = ({
+  checked = false,
+  className = '',
+  size = 20,
+  type = 'checkBox',
+  disabled = false,
+  ...rest
+}: CheckBoxProps) => {
   const inputId = useId();
   const checkedStyle = checked ? 'bg-black_900' : 'bg-gray_600';
+  const disabledStyle = disabled ? 'opacity-40' : 'hover:brightness-75';
 
   return (
     <>
       <input
         id={inputId}
-        type='checkbox'
+        type={type}
         checked={checked}
         style={{ display: 'none' }}
+        disabled={disabled}
         {...rest}
       />
       <motion.label
         htmlFor={inputId}
         className={twMerge(
-          twJoin('block rounded-[0.5rem] cursor-pointer hover:brightness-75', checkedStyle),
+          twJoin('block rounded-[0.5rem] cursor-pointer', checkedStyle, disabledStyle),
           className,
         )}
         style={{
           width: `${size * 0.1}rem`,
           height: `${size * 0.1}rem`,
         }}
-        whileTap={{ scale: 0.9, opacity: 0.6 }}
+        whileTap={disabled ? {} : { scale: 0.9, opacity: 0.6 }}
       >
         <CheckIcon
           fill='white'
