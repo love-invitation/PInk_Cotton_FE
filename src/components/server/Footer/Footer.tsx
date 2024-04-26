7894c9a414a71953ad229490213329c0db17c98c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 03:06:13 +0900

    🌱feat: Footer absolute option

--- src/components/server/Footer/Footer.tsx
+++ src/components/server/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FooterInfo, FooterNavigator } from './components';
 
 const Footer = () => {
   return (
-    <footer className='w-full h-[20rem] flex justify-center shadow-shadow_500'>
+    <footer className='w-full h-[20rem] flex justify-center shadow-shadow_500 absolute bottom-0'>
       <div className='w-full max-w-[100rem] min-w-[27rem] h-full flex flex-col justify-center gap-[1rem]'>
         <FooterNavigator />
         <Divider />
