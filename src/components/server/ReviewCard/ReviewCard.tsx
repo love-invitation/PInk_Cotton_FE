8e411feca0086d5bc7e2be842893c66cb871deea
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 01:02:09 +0900

    🌱feat: create ReviewCard Element & background

---
+++ src/components/server/ReviewCard/ReviewCard.tsx
@@ -0,0 +1,9 @@
+const ReviewCard = () => {
+  return (
+    <div className='w-[32rem] h-[18rem] rounded-[2.8rem] shadow-shadow_500 flex flex-col items-center'>
+      ReviewCard
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/components/server/index.ts
+++ src/components/server/index.ts
@@ -1,3 +1,4 @@
 export * from './icons';
 export { default as Divider } from './Divider/Divider';
 export { default as ReviewScore } from './ReviewScore/ReviewScore';
+export { default as ReviewCard } from './ReviewCard/ReviewCard';
