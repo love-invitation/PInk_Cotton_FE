5651c5e165d13ded6921c0719f71f6508cdfde47
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-26 15:36:46 +0900

    🌱feat: create Score's type & edit component

--- src/components/client/ReviewScoreAction/hooks/useChangeReviewScore/useChangeReviewScore.ts
+++ src/components/client/ReviewScoreAction/hooks/useChangeReviewScore/useChangeReviewScore.ts
@@ -2,12 +2,14 @@
 
 import { useState } from 'react';
 
+import { ScoreType } from '@/types/originType/Score';
+
 import { UseChangeReviewScoreProps } from './useChangeReviewScore.type';
 
 const useChangeReviewScore = ({ score, onClick }: UseChangeReviewScoreProps) => {
   const [currentScore, setCurrentScore] = useState(score);
 
-  const handleClickStar = (newScore: number) => {
+  const handleClickStar = (newScore: ScoreType) => {
     if (currentScore === 1 && newScore === currentScore) {
       setCurrentScore(0);
       onClick(0);
