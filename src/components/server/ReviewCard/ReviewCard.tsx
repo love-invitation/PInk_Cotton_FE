e17e3d31b2f446141b52e8b07c636cad818b9b0a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 01:23:08 +0900

    🌱feat: create ReviewCard's Top area

--- src/components/server/ReviewCard/ReviewCard.tsx
+++ src/components/server/ReviewCard/ReviewCard.tsx
@@ -1,7 +1,22 @@
-const ReviewCard = () => {
+import ReviewScore from '../ReviewScore/ReviewScore';
+import { ReviewCardProps } from './ReviewCard.type';
+
+const ReviewCard = ({ nickName, score }: ReviewCardProps) => {
+  const hideNickName = [
+    nickName[0],
+    '*'.repeat(nickName.length - 2),
+    nickName[nickName.length - 1],
+  ].join('');
+
   return (
-    <div className='w-[32rem] h-[18rem] rounded-[2.8rem] shadow-shadow_500 flex flex-col items-center'>
-      ReviewCard
+    <div className='w-[32rem] h-[18rem] rounded-[2.8rem] shadow-shadow_500 flex flex-col items-center p-[2rem]'>
+      <div className='w-full flex justify-between items-center'>
+        <p className='text-[1.2rem] font-semiBold text-gray_200 tracking-[0.24rem]'>
+          {hideNickName}
+          <small className='text-[1.2rem] font-semiBold text-gray_200 ml-1'>님</small>
+        </p>
+        <ReviewScore score={score} />
+      </div>
     </div>
   );
 };
