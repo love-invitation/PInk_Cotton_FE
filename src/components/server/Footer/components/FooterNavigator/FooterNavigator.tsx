5d9bb3120856a5a85aa0a83cc261f57fbc1c91af
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 02:35:59 +0900

    🌱feat: FooterNavigator style

--- src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
+++ src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+
+import Divider from '@/components/server/Divider/Divider';
+
 const FooterNavigator = () => {
-  return <div>FooterNavigator</div>;
+  return (
+    <nav className='w-full flex items-center'>
+      <h6 className='text-[1.2rem] font-bold mr-[2rem]'>PINKCOTTON</h6>
+
+      <ul className='flex flex-grow text-[1.2rem] text-gray_100'>
+        <li>
+          <Link href='/'>이용약관</Link>
+        </li>
+        <Divider direction='vertical' />
+        <li>
+          <Link href='/'>개인정보처리방침</Link>
+        </li>
+        <Divider direction='vertical' />
+        <li>
+          <Link href='/'>제휴문의</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default FooterNavigator;
