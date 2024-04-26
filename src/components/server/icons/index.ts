5694d76122b579f4f614704fbf1394ee994115b1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-26 14:03:14 +0900

    🌱feat: create StarIcon

--- src/components/server/icons/index.ts
+++ src/components/server/icons/index.ts
@@ -2,3 +2,4 @@ export { default as CenterIcon } from './CenterIcon/CenterIcon';
 export { default as CloseIcon } from './CloseIcon/CloseIcon';
 export { default as ArrowDownIcon } from './ArrowDownIcon/ArrowDownIcon';
 export { default as CheckIcon } from './CheckIcon/CheckIcon';
+export { default as StarIcon } from './StarIcon/StarIcon';
