19a2d73b6ab3270f85ef945354eedf4c1f72d141
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 01:45:43 +0900

    🎨style: detail Style

--- src/components/server/ReviewCard/ReviewCard.tsx
+++ src/components/server/ReviewCard/ReviewCard.tsx
@@ -6,20 +6,20 @@ const ReviewCard = ({ nickName, score, title, content }: ReviewCardProps) => {
   const newNickName = hideNickName(nickName);
 
   return (
-    <div className='w-[32.7rem] h-[18.6rem] rounded-[2.8rem] shadow-shadow_500 flex flex-col items-center pt-[2rem] pb-[1rem] px-[2rem] gap-[1.2rem]'>
+    <div className='w-[35rem] h-[24rem] rounded-[2.8rem] shadow-shadow_500 flex flex-col items-center pt-[3rem] pb-[1rem] px-[2rem] gap-[1.2rem] select-none'>
       <div className='w-full flex justify-between items-center'>
-        <p className='text-[1.2rem] font-semiBold text-gray_200 tracking-[0.24rem]'>
+        <p className='text-[1.6rem] font-semiBold text-gray_200 tracking-[0.18rem]'>
           {newNickName}
-          <small className='text-[1.2rem] font-semiBold text-gray_200 ml-1'>님</small>
+          <small className='text-[1.6rem] font-semiBold text-gray_200 ml-1'>님</small>
         </p>
         <ReviewScore score={score} />
       </div>
 
-      <h6 className='w-full text-[1.5rem] font-semiBold overflow-hidden text-nowrap text-ellipsis'>
+      <h6 className='w-full text-[1.8rem] font-semiBold overflow-hidden text-nowrap text-ellipsis mt-[1.4rem]'>
         {title}
       </h6>
 
-      <p className='w-full text-[1.3rem] font-medium text-gray_200 leading-[2.4rem] overflow-hidden line-clamp-3'>
+      <p className='w-full text-[1.6rem] font-medium text-gray_200 leading-[2.8rem] overflow-hidden line-clamp-3'>
         {content}
       </p>
     </div>
