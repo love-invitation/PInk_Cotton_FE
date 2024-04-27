ee3af1e5e5c5a30b83ec91afc7ac16cbccff0c43
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 11:54:35 +0900

    🛠️refactor: 공통 스타일 분리 및 애니메이션 추가

--- src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
+++ src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
@@ -3,26 +3,27 @@ import Link from 'next/link';
 import Divider from '@/components/server/Divider/Divider';
 
 const FooterNavigator = () => {
+  const liStyle = 'hover:brightness-75 hover:underline';
   return (
     <nav className='w-full flex items-center pl-[2rem] text-nowrap'>
       <h6 className='text-[1.2rem] font-bold mr-[2rem]'>PINKCOTTON</h6>
 
       <ul className='flex flex-grow text-[1.2rem] h-[1.8rem] items-center text-gray_100'>
-        <li>
+        <li className={liStyle}>
           <Link href='/'>이용약관</Link>
         </li>
         <Divider
           direction='vertical'
           className='h-[70%]'
         />
-        <li>
+        <li className={liStyle}>
           <Link href='/'>개인정보처리방침</Link>
         </li>
         <Divider
           direction='vertical'
           className='h-[70%]'
         />
-        <li>
+        <li className={liStyle}>
           <Link href='/'>제휴문의</Link>
         </li>
       </ul>
