5bfb9c21cd26b99234f6c381b766375f42d7b3e5
지인혁 <dhgg321@naver.com>
2024-04-28 01:34:02 +0900

    🎨style: Divider 색상 bg-gray-800으로 변경

--- src/components/server/Divider/Divider.tsx
+++ src/components/server/Divider/Divider.tsx
@@ -4,7 +4,7 @@ import { twJoin, twMerge } from 'tailwind-merge';
 
 const Divider = ({ direction = 'horizontal', className, ...rest }: DividerProps) => {
   const baseStyle = twJoin(
-    'border-none bg-gray_900',
+    'border-none bg-gray_800',
     direction === 'horizontal' && 'block h-px w-full',
     direction === 'vertical' && 'inline-block w-px h-full align-middle mx-[1rem]',
   );
