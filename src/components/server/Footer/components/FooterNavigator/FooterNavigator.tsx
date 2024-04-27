f9e9096d430fb86e8e23d01ea62d0ff29e878d4a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-27 11:55:40 +0900

    🎨style: add text animation transition

--- src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
+++ src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Divider from '@/components/server/Divider/Divider';
 
 const FooterNavigator = () => {
-  const liStyle = 'hover:brightness-75 hover:underline';
+  const liStyle = 'hover:brightness-75 hover:underline transition-all';
   return (
     <nav className='w-full flex items-center pl-[2rem] text-nowrap'>
       <h6 className='text-[1.2rem] font-bold mr-[2rem]'>PINKCOTTON</h6>
