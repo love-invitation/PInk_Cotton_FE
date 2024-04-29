3a39f23bad2840dad3a7258f5149f765472f8050
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 01:27:45 +0900

    🌱feat: HeaderNavbar

--- src/components/server/Header/Header.tsx
+++ src/components/server/Header/Header.tsx
@@ -2,7 +2,7 @@ import { HeaderLogo, HeaderNavbar, HeaderUserInfo } from './components';
 
 const Header = () => {
   return (
-    <header className='w-full h-[8.8rem] flex items-center justify-between'>
+    <header className='w-full h-[8.8rem] flex items-center justify-between shadow-shadow_500'>
       <HeaderLogo />
       <HeaderNavbar />
       <HeaderUserInfo />
--- src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
+++ src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
@@ -1,5 +1,52 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import Divider from '@/components/server/Divider/Divider';
+
+import { twJoin } from 'tailwind-merge';
+
 const HeaderNavbar = () => {
-  return <nav>HeaderNavbar</nav>;
+  const pathName = usePathname();
+
+  const introducePath = '/introduce';
+  const templatePath = '/template';
+  const customerPath = '/center';
+
+  return (
+    <nav>
+      <ul className='h-[5rem] text-size18 font-regular text-gray_500 flex items-center'>
+        <li
+          className={twJoin(pathName === introducePath && 'text-black_900', 'hover:brightness-90')}
+        >
+          <Link href={introducePath}>소개</Link>
+        </li>
+
+        <Divider
+          direction='vertical'
+          className='h-[30%] mx-[2rem]'
+        />
+
+        <li
+          className={twJoin(pathName === templatePath && 'text-black_900', 'hover:brightness-90')}
+        >
+          <Link href={templatePath}>모바일청첩장 종류</Link>
+        </li>
+
+        <Divider
+          direction='vertical'
+          className='h-[30%] mx-[2rem]'
+        />
+
+        <li
+          className={twJoin(pathName === customerPath && 'text-black_900', 'hover:brightness-90')}
+        >
+          <Link href={customerPath}>고객센터</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default HeaderNavbar;
