70d438f40b6d80dad420cc7b5650152cc6ab491c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 01:33:33 +0900

    🌱feat: create DotIcon

--- src/components/server/icons/index.ts
+++ src/components/server/icons/index.ts
@@ -3,3 +3,4 @@ export { default as CloseIcon } from './CloseIcon/CloseIcon';
 export { default as ArrowDownIcon } from './ArrowDownIcon/ArrowDownIcon';
 export { default as CheckIcon } from './CheckIcon/CheckIcon';
 export { default as StarIcon } from './StarIcon/StarIcon';
+export { default as DotIcon } from './DotIcon/DotIcon';
