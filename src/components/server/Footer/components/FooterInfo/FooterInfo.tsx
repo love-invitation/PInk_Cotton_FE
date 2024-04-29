ca669979748a906455dc893de35cec43da11da4d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 02:03:23 +0900

    🛠️refactor: create Company Info constants

--- src/components/server/Footer/components/FooterInfo/FooterInfo.tsx
+++ src/components/server/Footer/components/FooterInfo/FooterInfo.tsx
@@ -1,11 +1,9 @@
 import Divider from '@/components/server/Divider/Divider';
+import { COMPANY_INFO } from '@/constants';
 
 const FooterInfo = () => {
-  const email = 'abcdefg@pinkcotton.com';
-  const representative = '아무개';
-  const address = '서울시 용산구 어느빌딩';
-  const companyNumber = '1274812가124나';
-  const mailOrderBusinessReport = '79102731';
+  const { NAME, ADDRESS, REPRESENTATIVE, EMAIL, COMPANY_NUMBER, MAIL_ORDER_BUSINESS_REPORT } =
+    COMPANY_INFO;
 
   const liStyle = 'w-full h-[1.5rem] flex items-center';
 
@@ -13,31 +11,31 @@ const FooterInfo = () => {
     <ul className='w-full flex flex-col gap-[0.6rem] pl-[2rem] text-[1.1rem] text-gray_100 tracking-[0.05rem]'>
       <li className={liStyle}>
         <p>
-          상호명 : <strong>PINKCOTTON</strong>
+          상호명 : <strong>{NAME}</strong>
         </p>
         <Divider
           direction='vertical'
           className='h-[70%]'
         />
-        <p>{address}</p>
+        <p>{ADDRESS}</p>
       </li>
       <li className={liStyle}>
-        <p>대표자 : {representative}</p>
+        <p>대표자 : {REPRESENTATIVE}</p>
         <Divider
           direction='vertical'
           className='h-[70%]'
         />
-        <p>이메일 : {email}</p>
+        <p>이메일 : {EMAIL}</p>
       </li>
       <li className={liStyle}>
-        <p>사업자등록번호 : {companyNumber}</p>
+        <p>사업자등록번호 : {COMPANY_NUMBER}</p>
       </li>
       <li className={liStyle}>
-        <p>통신판매업신고 : {mailOrderBusinessReport}</p>
+        <p>통신판매업신고 : {MAIL_ORDER_BUSINESS_REPORT}</p>
       </li>
       <li className={liStyle}>
         <p>
-          Copyright ⓒ <strong>PINKCOTTON</strong> all rights reserved
+          Copyright ⓒ <strong>{NAME}</strong> all rights reserved
         </p>
       </li>
     </ul>
---
+++ src/constants/Company.ts
@@ -0,0 +1,9 @@
+export const COMPANY_INFO = {
+  NAME: 'PINKCOTTON',
+  EMAIL: 'abcdefg@pinkcotton.com',
+  REPRESENTATIVE: '아무개',
+  ADDRESS: '서울시 용산구 어느빌딩',
+  PHONE: '010-1234-5678',
+  MAIL_ORDER_BUSINESS_REPORT: '79102731',
+  COMPANY_NUMBER: '1234567890',
+};
