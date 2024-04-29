7d113e82ddf46edcf992f7e6ceda73c54059131b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 01:50:42 +0900

    🌱feat: changed HeaderNavbar's HeaderNavItem

--- src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
+++ src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
@@ -1,49 +1,48 @@
 'use client';
 
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import Divider from '@/components/server/Divider/Divider';
 
-import { twJoin } from 'tailwind-merge';
+import { HeaderNavItem } from './components';
 
 const HeaderNavbar = () => {
   const pathName = usePathname();
 
-  const introducePath = '/introduce';
-  const templatePath = '/template';
-  const customerPath = '/center';
+  const introduceHref = '/introduce';
+  const templateHref = '/template';
+  const customerHref = '/center';
 
   return (
     <nav>
       <ul className='h-[5rem] text-size18 font-regular text-gray_500 flex items-center'>
-        <li
-          className={twJoin(pathName === introducePath && 'text-black_900', 'hover:brightness-90')}
-        >
-          <Link href={introducePath}>소개</Link>
-        </li>
+        <HeaderNavItem
+          pathName={pathName}
+          href={introduceHref}
+          title='소개'
+        />
 
         <Divider
           direction='vertical'
           className='h-[30%] mx-[2rem]'
         />
 
-        <li
-          className={twJoin(pathName === templatePath && 'text-black_900', 'hover:brightness-90')}
-        >
-          <Link href={templatePath}>모바일청첩장 종류</Link>
-        </li>
+        <HeaderNavItem
+          pathName={pathName}
+          href={templateHref}
+          title='모바일청첩장 종류'
+        />
 
         <Divider
           direction='vertical'
           className='h-[30%] mx-[2rem]'
         />
 
-        <li
-          className={twJoin(pathName === customerPath && 'text-black_900', 'hover:brightness-90')}
-        >
-          <Link href={customerPath}>고객센터</Link>
-        </li>
+        <HeaderNavItem
+          pathName={pathName}
+          href={customerHref}
+          title='고객센터'
+        />
       </ul>
     </nav>
   );
