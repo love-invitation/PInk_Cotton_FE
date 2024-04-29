63c2ccc7fd80196cba397b260a2cd07368bb468b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 01:33:33 +0900

    🌱feat: create DotIcon

---
+++ src/components/server/icons/DotIcon/DotIcon.tsx
@@ -0,0 +1,24 @@
+import { DotIconProps } from './DotIcon.type';
+
+const DotIcon = ({ size = 5, fill = 'black', className = '', ...rest }: DotIconProps) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox='0 0 5 5'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+      {...rest}
+    >
+      <circle
+        cx='2.5'
+        cy='2.5'
+        r='2.5'
+        fill={fill}
+      />
+    </svg>
+  );
+};
+
+export default DotIcon;
