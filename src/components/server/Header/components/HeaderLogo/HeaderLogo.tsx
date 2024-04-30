cdd56d59dc664cd1360076daa2148b01755d44f8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 15:36:35 +0900

    🛠️refactor: code convention

--- src/components/server/Header/components/HeaderLogo/HeaderLogo.tsx
+++ src/components/server/Header/components/HeaderLogo/HeaderLogo.tsx
@@ -1,7 +1,13 @@
 import style from './HeaderLogo.style.module.css';
 
 const HeaderLogo = () => {
-  return <div className={style.header_logo}>Logo place</div>;
+  return (
+    <div
+      className={`ml-[4%] flex items-center justify-center border-red-500 border-[1px] ${style.header_logo}`}
+    >
+      Logo place
+    </div>
+  );
 };
 
 export default HeaderLogo;
