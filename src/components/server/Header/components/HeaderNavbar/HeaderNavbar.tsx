8bf96d6b49cbb7515489184494d8621e38ee6f64
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 15:13:35 +0900

    🎨style: edited HeaderNavbar's responsive

--- src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
+++ src/components/server/Header/components/HeaderNavbar/HeaderNavbar.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import style from './HeaderNavbar.style.module.css';
+
 import { usePathname } from 'next/navigation';
 
 import Divider from '@/components/server/Divider/Divider';
@@ -14,8 +16,8 @@ const HeaderNavbar = () => {
   const customerHref = '/center';
 
   return (
-    <nav className='mr-[6%]'>
-      <ul className='h-[5rem] text-size18 font-normal text-gray_500 flex items-center text-nowrap'>
+    <nav className={style.nav}>
+      <ul className={`text-gray_500 flex items-center text-nowrap font-regular ${style.nav_list}`}>
         <HeaderNavItem
           pathName={pathName}
           href={introduceHref}
