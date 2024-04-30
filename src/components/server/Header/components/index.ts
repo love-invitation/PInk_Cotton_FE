168175a6cbbb46ab4450f86400afa0f6634b84cb
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 14:19:29 +0900

    🌱feat: separate Header None Response Component

--- src/components/server/Header/components/index.ts
+++ src/components/server/Header/components/index.ts
@@ -1,3 +1,4 @@
 export { default as HeaderLogo } from './HeaderLogo/HeaderLogo';
 export { default as HeaderNavbar } from './HeaderNavbar/HeaderNavbar';
 export { default as HeaderUserInfo } from './HeaderUserInfo/HeaderUserInfo';
+export { default as HeaderNoneResponsive } from './HeaderNoneResponsive/HeaderNoneResponsive';
