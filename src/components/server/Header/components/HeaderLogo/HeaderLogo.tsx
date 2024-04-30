b15f8009347ce78963e9823eb0ff7e1d5621ad74
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-30 19:17:45 +0900

    🌱feat: reflected Logo in HeaderLogo

--- src/components/server/Header/components/HeaderLogo/HeaderLogo.tsx
+++ src/components/server/Header/components/HeaderLogo/HeaderLogo.tsx
@@ -1,11 +1,11 @@
 import style from './HeaderLogo.style.module.css';
 
+import { Logo } from '@/components/server/';
+
 const HeaderLogo = () => {
   return (
-    <div
-      className={`ml-[4%] flex items-center justify-center border-red-500 border-[1px] ${style.header_logo}`}
-    >
-      Logo place
+    <div className={`ml-[4%] flex items-center justify-center relative ${style.header_logo}`}>
+      <Logo />
     </div>
   );
 };
