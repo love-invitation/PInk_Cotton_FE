b4f23a0cacde669741094cdcbff99524b8bdf906
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-01 01:04:39 +0900

    🛠️refactor: refactor CarouselImageItem

--- src/components/server/Carousel/components/CarouselImageItem/CarouselImageItem.tsx
+++ src/components/server/Carousel/components/CarouselImageItem/CarouselImageItem.tsx
@@ -5,15 +5,17 @@ import { CarouselImageItemProps } from './CarouselImageItem.type';
 const CarouselImageItem = ({ src, index }: CarouselImageItemProps) => {
   return (
     <li
-      id={`img_${index}`}
+      // TODO
+      // id값 추후 고려
+      id={`photo_${index}`}
       className='carousel-item w-full relative'
       key={src}
     >
       <Image
         src={src}
-        alt='carousel_image'
+        alt={`이미지 ${index}`}
         fill
-        priority
+        sizes='100%'
         style={{
           objectFit: 'contain',
         }}
