5d1d747ebdd1eb276adb425bc2d3bc48b1bc3bc9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-01 01:05:59 +0900

    🌱feat: create Carousel component

---
+++ src/components/server/Carousel/Carousel.tsx
@@ -0,0 +1,23 @@
+import { CarouselProps } from './Carousel.type';
+import { CarouselImageItem } from './components';
+
+import { twMerge } from 'tailwind-merge';
+
+const Carousel = ({ srcList, width = '100%', height = '100%', className = '' }: CarouselProps) => {
+  return (
+    <ul
+      className={twMerge('carousel border-2 bg-black', className)}
+      style={{ width, height }}
+    >
+      {srcList &&
+        srcList.map((src, index) => (
+          <CarouselImageItem
+            src={src}
+            index={index}
+          />
+        ))}
+    </ul>
+  );
+};
+
+export default Carousel;
