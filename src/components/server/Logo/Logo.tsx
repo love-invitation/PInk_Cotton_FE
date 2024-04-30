b126a5bdebd68f0f218f6f1aa1ba90fd302741f4
지인혁 <dhgg321@naver.com>
2024-04-30 17:35:57 +0900

    🌱feat: Logo 컴포넌트 기능 개발

---
+++ src/components/server/Logo/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import LogoImage from '@/assets/images/logo.png';
+
+import { LogoProps } from './Logo.type';
+
+const Logo = ({ ...rest }: LogoProps) => {
+  return (
+    <Link href='/'>
+      <Image
+        {...rest}
+        src={LogoImage}
+        alt='웹 사이트 로고'
+      />
+    </Link>
+  );
+};
+
+export default Logo;
