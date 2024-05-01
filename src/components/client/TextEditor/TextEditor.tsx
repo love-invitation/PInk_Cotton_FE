4989ba5bd0a6b9b83ec06ae491837b0b61accefa
지인혁 <dhgg321@naver.com>
2024-05-02 01:29:21 +0900

    🌱feat: TextEditor 컴포넌트 기능 개발

---
+++ src/components/client/TextEditor/TextEditor.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import 'react-quill/dist/quill.snow.css';
+
+import { useFormContext } from 'react-hook-form';
+
+import dynamic from 'next/dynamic';
+
+import { TextEditorProps } from './TextEditor.type';
+import { modules } from './constants';
+
+import { twMerge } from 'tailwind-merge';
+
+const ReactQuill = dynamic(() => import('react-quill'), {
+  ssr: false,
+});
+
+const TextEditor = ({ name = 'content', className }: TextEditorProps) => {
+  const { watch, setValue } = useFormContext();
+
+  return (
+    <ReactQuill
+      theme='snow'
+      className={twMerge('h-[22.3rem] overflow-scroll border', className)}
+      modules={modules}
+      value={watch(name)}
+      onChange={(newContent: string) => setValue(name, newContent)}
+    />
+  );
+};
+
+export default TextEditor;
