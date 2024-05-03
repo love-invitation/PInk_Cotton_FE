bd7b338b8c9856823f22aa1fe8e786a379f7fd6d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 19:45:50 +0900

    🎨style: change percent sizing

--- src/components/server/TemplateCard/components/PhoneMockUp/PhoneMockup.tsx
+++ src/components/server/TemplateCard/components/PhoneMockUp/PhoneMockup.tsx
@@ -4,14 +4,14 @@ import { PhoneCameraIcon, PhoneSpeakerIcon } from './components';
 const PhoneMockup = ({ children }: PhoneMockupProps) => {
   return (
     <div className='w-full h-full px-[0.2rem] relative'>
-      <div className='w-[1rem] h-[8rem] bg-[#121515] absolute top-[18rem] right-0' />
-      <div className='w-[1rem] h-[3rem] bg-[#121515] absolute top-[13rem] left-0' />
-      <div className='w-[1rem] h-[5rem] bg-[#121515] absolute top-[17rem] left-0' />
-      <div className='w-[1rem] h-[5rem] bg-[#121515] absolute top-[23rem] left-0' />
+      <div className='w-[1rem] h-[12%] bg-[#121515] absolute top-[27%] right-0' />
+      <div className='w-[1rem] h-[5%] bg-[#121515] absolute top-[18.5%] left-0' />
+      <div className='w-[1rem] h-[7%] bg-[#121515] absolute top-[25.5%] left-0' />
+      <div className='w-[1rem] h-[7%] bg-[#121515] absolute top-[33.5%] left-0' />
       <div className='w-full h-full border-[1.4rem] border-[#121515] rounded-radius40 relative flex justify-center overflow-hidden'>
-        <div className='absolute w-[50%] h-[2.2rem] bg-[#121515] rounded-b-radius10 flex justify-center top-[-0.2rem]'>
-          <PhoneSpeakerIcon className='relative top-[0.4rem]' />
-          <PhoneCameraIcon className='absolute right-[4rem] top-[0.2rem]' />
+        <div className='absolute w-[50%] h-[3.6%] bg-[#121515] rounded-b-radius10 flex justify-center top-[0%]'>
+          <PhoneSpeakerIcon className='relative top-[20%]' />
+          <PhoneCameraIcon className='absolute right-[25%] top-[10%]' />
         </div>
         {children}
       </div>
