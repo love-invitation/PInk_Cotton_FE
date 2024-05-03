e7dcc0741ff62c1975aac3fd2e30a112827e6c66
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 16:01:19 +0900

    🌱feat: create TemplateCard component

---
+++ src/components/server/TemplateCard/TemplateCard.tsx
@@ -0,0 +1,21 @@
+import { TemplateCardProps } from './TemplateCard.type';
+import { useCardSize } from './hooks';
+
+import { twJoin, twMerge } from 'tailwind-merge';
+
+const TemplateCard = ({ children, size = 'medium', className }: TemplateCardProps) => {
+  const cardSize = useCardSize({ type: size });
+
+  return (
+    <figure
+      className={twMerge(
+        twJoin('w-[11.5rem] h-[20.2rem] shadow-shadow_500 rounded-radius20', cardSize),
+        className,
+      )}
+    >
+      {children}
+    </figure>
+  );
+};
+
+export default TemplateCard;
--- src/components/server/index.ts
+++ src/components/server/index.ts
@@ -9,3 +9,4 @@ export { default as Carousel } from './Carousel/Carousel';
 export { default as EditorView } from './EditorView/EditorView';
 export { default as CommonList } from './CommonList/CommonList';
 export { default as Banner } from './Banner/Banner';
+export { default as TemplateCard } from './TemplateCard/TemplateCard';
