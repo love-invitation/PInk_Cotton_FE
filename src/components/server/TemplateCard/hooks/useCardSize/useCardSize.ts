d96ee79949a6f5ae3ad0ac3d74561aa08ebb65b9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 19:14:06 +0900

    🌱feat: added PhoneMockup in TemplateCard component

--- src/components/server/TemplateCard/hooks/useCardSize/useCardSize.ts
+++ src/components/server/TemplateCard/hooks/useCardSize/useCardSize.ts
@@ -3,19 +3,19 @@ import { UseCardSize } from './useCardSize.type';
 const useCardSize = ({ type }: UseCardSize) => {
   switch (type) {
     case 'small':
-      return 'w-[11.4rem] h-[20.2rem]';
+      return 'w-[11.4rem] h-[20.2rem] rounded-radius20 shadow-shadow_500';
 
     case 'medium':
-      return 'w-[22.7rem] h-[40.1rem]';
+      return 'w-[22.7rem] h-[40.1rem] rounded-radius20 shadow-shadow_500';
 
     case 'large':
-      return 'w-[41.5rem] h-[81.3rem]';
+      return 'w-[41.5rem] h-[81.3rem] rounded-radius20 shadow-shadow_500';
 
     case 'mockup':
-      return 'w-[32.6rem] h-[68.5rem]';
+      return 'w-[32.6rem] h-[68.5rem] rounded-radius40';
 
     default:
-      return 'w-[22.7rem] h-[40.1rem]';
+      return 'w-[22.7rem] h-[40.1rem] rounded-radius20 shadow-shadow_500';
   }
 };
 
