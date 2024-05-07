5e53fc7219cf43fe3d837cce08e5c003d5976c6a
지인혁 <dhgg321@naver.com>
2024-05-07 18:44:19 +0900

    🛠️refactor: DropdownSelect button -> p 태그로 변경

--- src/components/client/Dropdown/DropdownSelect/DropdownSelect.type.ts
+++ src/components/client/Dropdown/DropdownSelect/DropdownSelect.type.ts
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from 'react';
 
-export interface DropdownSelectProps extends HTMLAttributes<HTMLButtonElement> {
+export interface DropdownSelectProps extends HTMLAttributes<HTMLParagraphElement> {
   className?: string;
 }
