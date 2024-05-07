f9821c54f0eb5615b86eb0af609fe1e6c77fa70a
지인혁 <dhgg321@naver.com>
2024-05-07 18:44:19 +0900

    🛠️refactor: DropdownSelect button -> p 태그로 변경

--- src/components/client/Dropdown/DropdownSelect/DropdownSelect.tsx
+++ src/components/client/Dropdown/DropdownSelect/DropdownSelect.tsx
@@ -7,13 +7,12 @@ const DropdownSelect = ({ className, ...rest }: DropdownSelectProps) => {
   const { selectedValue } = useDropdownContext();
 
   return (
-    <button
-      type='button'
-      className={twMerge('text-left w-full h-full px-[1.6rem] py-[1.2rem]', className)}
+    <p
+      className={twMerge('text-left h-full px-[1.6rem] py-[1.2rem]', className)}
       {...rest}
     >
       {selectedValue}
-    </button>
+    </p>
   );
 };
 
