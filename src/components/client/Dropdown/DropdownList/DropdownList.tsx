b01995e98e22b4026a477837c20b52d73e075afb
지인혁 <dhgg321@naver.com>
2024-05-08 19:59:07 +0900

    🛠️refactor: DropdownList CommonList 컴포넌트로 리스트 변경

--- src/components/client/Dropdown/DropdownList/DropdownList.tsx
+++ src/components/client/Dropdown/DropdownList/DropdownList.tsx
@@ -1,29 +1,35 @@
 import { MouseEvent } from 'react';
 
+import { CommonList } from '@/components/server';
+
 import { useDropdownContext } from '../context/DropdownContext/DropdownContext';
 import { DropdownListProps } from './DropdownList.type';
 
 import { twJoin, twMerge } from 'tailwind-merge';
 
 const DropdownList = ({ list, ulClassName, buttonClassName }: DropdownListProps) => {
-  const { isVisible, handleValueChange } = useDropdownContext();
+  const { isToggle, handleValueChange } = useDropdownContext();
 
   return (
-    <ul className={twMerge(twJoin(isVisible && 'block', !isVisible && 'hidden'), ulClassName)}>
-      {list.map((value) => (
-        <li key={value}>
-          <button
-            type='button'
-            className={twMerge(
-              'text-left w-full h-full hover:text-pink_500 px-[1.6rem] py-[1.2rem]',
-              buttonClassName,
-            )}
-            onClick={(event: MouseEvent<HTMLButtonElement>) => handleValueChange(event, value)}
-          >
-            {value}
-          </button>
-        </li>
-      ))}
+    <ul className={twMerge(twJoin(isToggle && 'block', !isToggle && 'hidden'), ulClassName)}>
+      <CommonList data={list}>
+        {(value) => {
+          return (
+            <li key={value}>
+              <button
+                type='button'
+                className={twMerge(
+                  'text-left w-full h-full hover:text-pink_500 px-[1.6rem] py-[1.2rem]',
+                  buttonClassName,
+                )}
+                onClick={(event: MouseEvent<HTMLButtonElement>) => handleValueChange(event, value)}
+              >
+                {value}
+              </button>
+            </li>
+          );
+        }}
+      </CommonList>
     </ul>
   );
 };
