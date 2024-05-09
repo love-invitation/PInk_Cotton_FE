9168edea0e31e94651051bfaacc0d237b6a734ce
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-09 15:12:08 +0900

    🗑️remove: DropDown Test Code 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-'use client';
-
-import { Dropdown } from '@/components/client';
-
 const Home = () => {
-  const list = ['선택 없음', '1', '2', '3'];
-
-  return (
-    <main className='h-full w-full'>
-      <Dropdown
-        defaultValue={list[0]}
-        onChange={() => {}}
-      >
-        <Dropdown.Select />
-        <Dropdown.List list={list} />
-      </Dropdown>
-    </main>
-  );
+  return <main className='h-full w-full'>Pink Cotton</main>;
 };
 
 export default Home;
