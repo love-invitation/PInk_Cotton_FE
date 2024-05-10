95c43d23e8628521a66881c0fe27b403324c7901
지인혁 <dhgg321@naver.com>
2024-05-10 23:55:01 +0900

    🌱feat: 경기도바탕 폰트 파일 추가

---
+++ src/assets/font/GyeonggiBatang.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local';
+
+const GyeonggiBatangBold = localFont({
+  src: '../../../public/fonts/Batang_Bold.woff',
+  display: 'swap',
+});
+
+export default GyeonggiBatangBold;
