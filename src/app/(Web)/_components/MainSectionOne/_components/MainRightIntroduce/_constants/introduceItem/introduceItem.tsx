fa11204c13a370d417d8e71153508722a3ab2b78
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 14:34:56 +0900

    🌱feat: Main Introduce Item 상수 생성

---
+++ src/app/(Web)/_components/MainSectionOne/_components/MainRightIntroduce/_constants/introduceItem/introduceItem.tsx
@@ -0,0 +1,26 @@
+import { Clock3DIcon, Folder3DIcon, Paint3DIcon, Send3DIcon } from '@/components/server';
+
+const INTRODUCE_LIST = [
+  {
+    icon: <Clock3DIcon size={80} />,
+    title: '5분이면 충분해요!',
+    description: '포대장 제목, 인사말, 웨딩 사진까지 쉽고 빠르게 완성하는 청첩장',
+  },
+  {
+    icon: <Folder3DIcon size={80} />,
+    title: '필요한 모든 기능이 제공돼요!',
+    description: '웨딩사진 / 예식장 위치 / 달력 / 전화,문자 / 축의금 등',
+  },
+  {
+    icon: <Send3DIcon size={80} />,
+    title: '제작 즉시 전송 가능해요!',
+    description: '제작부터 결제, 전송까지 막힘없이! 가족 및 친구에게 바로 전송 가능!',
+  },
+  {
+    icon: <Paint3DIcon size={80} />,
+    title: '다양한 템플릿이 제공돼요!',
+    description: 'PINK COTTON만의 여러 디자인을 경험해보세요.',
+  },
+];
+
+export default INTRODUCE_LIST;
