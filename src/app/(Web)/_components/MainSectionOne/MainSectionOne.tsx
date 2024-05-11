da90187f1314187fda47b08870b59367f6664078
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 14:49:02 +0900

    🎨style: width 1050에 대한 반응형 구현

--- src/app/(Web)/_components/MainSectionOne/MainSectionOne.tsx
+++ src/app/(Web)/_components/MainSectionOne/MainSectionOne.tsx
@@ -4,7 +4,7 @@ const MainSectionOne = () => {
   return (
     <section
       id='calc_header_height'
-      className='w-full flex justify-center items-center'
+      className='w-full flex justify-center items-center main_page_1050:gap-[2rem] main_page_1050:min-h-[120rem]'
     >
       <MainLeftIntroduce />
       <MainRightIntroduce />
--- src/app/(Web)/_components/MainSectionOne/_components/MainLeftIntroduce/MainLeftIntroduce.tsx
+++ src/app/(Web)/_components/MainSectionOne/_components/MainLeftIntroduce/MainLeftIntroduce.tsx
@@ -2,8 +2,8 @@ import { LayerIcon, PhoneMockup } from '@/components/server';
 
 const MainLeftIntroduce = () => {
   return (
-    <article className='border-2 border-red-300 flex justify-center items-end gap-[0.3rem] select-none'>
-      <div className='border-2 border-green-300 flex flex-col items-center gap-[3rem]'>
+    <article className='hidden justify-center items-end gap-[0.3rem] select-none main_page_1050:flex py-[2rem]'>
+      <div className='flex flex-col items-center gap-[3rem]'>
         <PhoneMockup>
           {
             /*
@@ -14,7 +14,7 @@ const MainLeftIntroduce = () => {
             null
           }
         </PhoneMockup>
-        <p className='text-size16 font-regular text-gray-400 mb-[2rem]'>화면을 내려보세요!</p>
+        <p className='text-size16 font-regular text-gray-400'>화면을 내려보세요!</p>
       </div>
 
       <LayerIcon />
