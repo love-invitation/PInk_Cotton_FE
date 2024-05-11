2f1349bef6be385536a84884e0b3e980a33d5c81
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 13:42:14 +0900

    🌱feat: MainLeftIntroduce 구현

--- src/app/(Web)/_components/MainSectionOne/_components/MainLeftIntroduce/MainLeftIntroduce.tsx
+++ src/app/(Web)/_components/MainSectionOne/_components/MainLeftIntroduce/MainLeftIntroduce.tsx
@@ -1,5 +1,25 @@
+import { LayerIcon, PhoneMockup } from '@/components/server';
+
 const MainLeftIntroduce = () => {
-  return <div>MainLeftIntroduce</div>;
+  return (
+    <article className='border-2 border-red-300 flex justify-center items-end gap-[0.3rem] select-none'>
+      <div className='border-2 border-green-300 flex flex-col items-center gap-[3rem]'>
+        <PhoneMockup>
+          {
+            /*
+              TODO
+
+              추후 Template 넣기
+            */
+            null
+          }
+        </PhoneMockup>
+        <p className='text-size16 font-regular text-gray-400 mb-[2rem]'>화면을 내려보세요!</p>
+      </div>
+
+      <LayerIcon />
+    </article>
+  );
 };
 
 export default MainLeftIntroduce;
