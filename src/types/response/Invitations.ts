57602c6a22eebdee6e7f6dd721d250037afb4b95
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 18:05:39 +0900

    🌱feat: Invitations Type 생성

---
+++ src/types/response/Invitations.ts
@@ -0,0 +1,19 @@
+export interface BestInvitationsResponse {
+  status: number;
+  message: string;
+  result: {
+    title: string;
+    detail: string;
+    groomName: string;
+    brideName: string;
+    productInfoList: BestInvitationsItem[];
+  };
+}
+
+export interface BestInvitationsItem {
+  imageUrl: string;
+  templateName: string;
+  price: number;
+  best: boolean;
+  newest: boolean;
+}
