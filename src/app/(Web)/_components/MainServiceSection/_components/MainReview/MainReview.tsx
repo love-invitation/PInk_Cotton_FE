299c587d3cec03be46ba10e75b53523fceef4e44
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 23:41:42 +0900

    🛠️refactor: 시맨틱 태그 및 반응형 개선

--- src/app/(Web)/_components/MainServiceSection/_components/MainReview/MainReview.tsx
+++ src/app/(Web)/_components/MainServiceSection/_components/MainReview/MainReview.tsx
@@ -3,14 +3,14 @@ import { ReviewCard } from '@/components/server';
 const MainReview = () => {
   return (
     <article className='flex flex-col gap-[4rem] items-center main_page_1050:items-start'>
-      <span className='flex flex-col'>
+      <header className='flex flex-col'>
         <h3 className='text-size20 main_page_1050:text-size30 font-semiBold text-nowrap'>
           증명된 고객후기
         </h3>
         <p className='text-size13 main_page_1050:text-size16 text-gray_400'>
           PINK COTTON과 함께한 고객분들의 솔직한 후기입니다.
         </p>
-      </span>
+      </header>
 
       <ul className='flex gap-[2rem] main_page_1050:flex-row flex-col items-center'>
         <li>
