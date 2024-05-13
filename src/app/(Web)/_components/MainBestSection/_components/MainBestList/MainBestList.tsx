2d7fdc82a4a8b3fea58b27b22ded26422df9adcf
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 19:37:20 +0900

    🌱feat: MainBestList Props 생성 및 API 연결

--- src/app/(Web)/_components/MainBestSection/_components/MainBestList/MainBestList.tsx
+++ src/app/(Web)/_components/MainBestSection/_components/MainBestList/MainBestList.tsx
@@ -1,33 +1,51 @@
 import { TemplateItem } from '@/components/server';
 
-const MainBestList = () => {
+import { MainBestListProps } from './MainBestList.type';
+
+const MainBestList = ({ invitation }: MainBestListProps) => {
   return (
-    <article className='w-full main_page_550:max-w-[117rem] flex gap-[2rem] overflow-scroll scrollbar-hide items-center justify-center'>
-      <TemplateItem
-        name='name'
-        id='id'
-        isNew
-        price={40000}
-      >
-        {null}
-      </TemplateItem>
-      <TemplateItem
-        name='name'
-        id='id'
-        isNew
-        price={40000}
-      >
-        {null}
-      </TemplateItem>
-      <TemplateItem
-        name='name'
-        id='id'
-        isNew
-        price={40000}
-      >
-        {null}
-      </TemplateItem>
-    </article>
+    <ul className='w-full main_page_550:max-w-[117rem] flex gap-[2rem] overflow-scroll scrollbar-hide items-center justify-center'>
+      {invitation.map(({ templateName, newest, price }) => (
+        <TemplateItem
+          name={templateName}
+          isNew={newest}
+          price={price}
+          id={templateName}
+        >
+          {templateName}
+        </TemplateItem>
+      ))}
+
+      {/* <li>
+        <TemplateItem
+          name='ddd'
+          price={40000}
+          id='2fds32'
+        >
+          #33
+        </TemplateItem>
+      </li>
+
+      <li>
+        <TemplateItem
+          name='ddd'
+          price={40000}
+          id='2fds32'
+        >
+          #33
+        </TemplateItem>
+      </li>
+
+      <li>
+        <TemplateItem
+          name='ddd'
+          price={40000}
+          id='2fds32'
+        >
+          #33
+        </TemplateItem>
+      </li> */}
+    </ul>
   );
 };
 
