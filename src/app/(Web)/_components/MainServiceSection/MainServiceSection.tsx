7098af8a414e1a4b0204737b46b849db47d19d79
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 23:31:04 +0900

    🌱feat: 임시 고객센터란 제작 및 page 반응형 조정

--- src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
+++ src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
@@ -4,13 +4,26 @@ const MainServiceSection = () => {
   return (
     <section
       id='calc_header_height'
-      className='w-full py-[4rem] flex flex-col items-center justify-center gap-[6rem] px-[1.6rem] text-black_500'
+      className='w-full px-[1.6rem] py-[6rem] flex items-center justify-center'
     >
-      <MainReview />
+      <article className='flex flex-col gap-[10rem]'>
+        <MainReview />
 
-      <div className='w-full flex justify-center'>
-        <MainQuestion />
-      </div>
+        <div className='w-full flex flex-col main_page_1050:flex-row gap-[10rem] main_page_1050:gap-0 justify-between'>
+          <MainQuestion />
+
+          <div className='flex flex-col gap-[4rem]'>
+            <span>
+              <h3 className='text-size30 font-semiBold text-nowrap'>고객센터</h3>
+              <p className='text-size16 text-gray_400 break-keep'>
+                더 궁금한 점이 있으신가요? 아래 버튼을 눌러주세요.
+              </p>
+            </span>
+
+            <div>카카오톡 연결</div>
+          </div>
+        </div>
+      </article>
     </section>
   );
 };
