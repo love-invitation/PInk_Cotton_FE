0484bac94a0432e8bf0db9625faf7415f9deeef3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 19:48:27 +0900

    🌱feat: Animation 추가

--- src/app/(Web)/_components/MainBestSection/_components/MainBestList/MainBestList.tsx
+++ src/app/(Web)/_components/MainBestSection/_components/MainBestList/MainBestList.tsx
@@ -1,50 +1,43 @@
+'use client';
+
+import { useRef } from 'react';
+
 import { TemplateItem } from '@/components/server';
 
 import { MainBestListProps } from './MainBestList.type';
 
+import { AnimatePresence, motion, useInView } from 'framer-motion';
+
 const MainBestList = ({ invitation }: MainBestListProps) => {
+  const ref = useRef<HTMLUListElement | null>(null);
+  const isInView = useInView(ref);
+
   return (
-    <ul className='w-full main_page_550:max-w-[117rem] flex gap-[2rem] overflow-scroll scrollbar-hide items-center justify-center'>
-      {invitation.map(({ templateName, newest, price }) => (
-        <TemplateItem
-          name={templateName}
-          isNew={newest}
-          price={price}
-          id={templateName}
-        >
-          {templateName}
-        </TemplateItem>
-      ))}
-
-      {/* <li>
-        <TemplateItem
-          name='ddd'
-          price={40000}
-          id='2fds32'
-        >
-          #33
-        </TemplateItem>
-      </li>
-
-      <li>
-        <TemplateItem
-          name='ddd'
-          price={40000}
-          id='2fds32'
-        >
-          #33
-        </TemplateItem>
-      </li>
-
-      <li>
-        <TemplateItem
-          name='ddd'
-          price={40000}
-          id='2fds32'
-        >
-          #33
-        </TemplateItem>
-      </li> */}
+    <ul
+      ref={ref}
+      className='w-full main_page_550:max-w-[117rem] flex gap-[2rem] overflow-scroll scrollbar-hide items-center justify-center'
+    >
+      <AnimatePresence>
+        {isInView &&
+          invitation.map(({ templateName, newest, price }, index) => (
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1, delay: index * 0.5 }}
+              key={templateName}
+            >
+              <TemplateItem
+                name={templateName}
+                isNew={newest}
+                price={price}
+                id={templateName}
+              >
+                {templateName}
+              </TemplateItem>
+            </motion.li>
+          ))}
+      </AnimatePresence>
     </ul>
   );
 };
