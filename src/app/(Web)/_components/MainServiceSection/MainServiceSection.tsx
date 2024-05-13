c1b573d796d8761de1efb6e41eb09cb590aba41b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-13 23:41:42 +0900

    🛠️refactor: 시맨틱 태그 및 반응형 개선

--- src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
+++ src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
@@ -4,26 +4,28 @@ const MainServiceSection = () => {
   return (
     <section
       id='calc_header_height'
-      className='w-full px-[1.6rem] py-[6rem] flex items-center justify-center'
+      className='w-full flex items-center justify-center'
     >
-      <article className='flex flex-col gap-[10rem]'>
+      <div className='px-[2rem] py-[6rem] flex flex-col gap-[10rem]'>
         <MainReview />
 
         <div className='w-full flex flex-col main_page_1050:flex-row gap-[10rem] main_page_1050:gap-0 justify-between'>
           <MainQuestion />
 
-          <div className='flex flex-col gap-[4rem]'>
-            <span>
-              <h3 className='text-size30 font-semiBold text-nowrap'>고객센터</h3>
-              <p className='text-size16 text-gray_400 break-keep'>
+          <article className='flex flex-col gap-[4rem] items-center main_page_1050:items-start'>
+            <header>
+              <h3 className='text-size20 main_page_1050:text-size30 font-semiBold text-nowrap'>
+                고객센터
+              </h3>
+              <p className='text-size13 main_page_1050:text-size16 text-gray_400 break-keep'>
                 더 궁금한 점이 있으신가요? 아래 버튼을 눌러주세요.
               </p>
-            </span>
+            </header>
 
             <div>카카오톡 연결</div>
-          </div>
+          </article>
         </div>
-      </article>
+      </div>
     </section>
   );
 };
