4a5bc23f17608599f57431cf42d51dc99be849ae
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-14 10:54:02 +0900

    🛠️refactor: 중복 Component 컴포넌트로 분리 적용

--- src/app/(Web)/_components/MainServiceSection/_components/MainQuestion/MainQuestion.tsx
+++ src/app/(Web)/_components/MainServiceSection/_components/MainQuestion/MainQuestion.tsx
@@ -1,19 +1,16 @@
 import { Accordion } from '@/components/client';
 import { Divider } from '@/components/server';
 
+import MainSectionTitle from '../../../MainSectionTitle/MainSectionTitle';
 import { MAIN_QUESTION_LIST } from './constants';
 
 const MainQuestion = () => {
   return (
     <article className='flex flex-col gap-[4rem] grow-1 items-center main_page_1050:items-start'>
-      <header className='flex flex-col'>
-        <h3 className='text-size20 main_page_1050:text-size30 font-semiBold text-nowrap'>
-          자주 묻는 질문
-        </h3>
-        <p className='text-size13 main_page_1050:text-size16 text-gray_400'>
-          자주 들어오는 질문입니다. 더 궁금한 점이 있다면 카카오톡채널을 이용해주세요!
-        </p>
-      </header>
+      <MainSectionTitle
+        title='자주 묻는 질문'
+        subTitle='자주 들어오는 질문입니다. 더 궁금한 점이 있다면 카카오톡채널을 이용해주세요!'
+      />
 
       <ul>
         {MAIN_QUESTION_LIST.map(({ question, answer }) => (
