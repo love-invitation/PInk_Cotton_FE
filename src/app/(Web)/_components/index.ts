a9dfbe284cc82c52ee7cb5dedcc8cfe6943ca238
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-14 10:53:35 +0900

    🌱feat: MainSectionTitle 생성

--- src/app/(Web)/_components/index.ts
+++ src/app/(Web)/_components/index.ts
@@ -1,3 +1,4 @@
 export { default as MainIntroSection } from './MainIntroSection/MainIntroSection';
 export { default as MainBestSection } from './MainBestSection/MainBestSection';
 export { default as MainServiceSection } from './MainServiceSection/MainServiceSection';
+export { default as MainSectionTitle } from './MainSectionTitle/MainSectionTitle';
