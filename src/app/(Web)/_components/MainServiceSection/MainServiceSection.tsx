d85f8e42b787682a80b65cac198dd8942ed53a4f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-14 11:05:11 +0900

    🌱feat: Service Component 분리 및 Modal 연결

--- src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
+++ src/app/(Web)/_components/MainServiceSection/MainServiceSection.tsx
@@ -1,7 +1,4 @@
-import { KakaoChannelIcon } from '@/components/server';
-
-import MainSectionTitle from '../MainSectionTitle/MainSectionTitle';
-import { MainQuestion, MainReview } from './_components';
+import { MainQuestion, MainReview, MainService } from './_components';
 
 const MainServiceSection = () => {
   return (
@@ -15,23 +12,7 @@ const MainServiceSection = () => {
         <div className='w-full flex flex-col main_page_1050:flex-row gap-[10rem] main_page_1050:gap-0 justify-between'>
           <MainQuestion />
 
-          <article className='flex flex-col gap-[4rem] items-center main_page_1050:items-start'>
-            <MainSectionTitle
-              title='고객센터'
-              subTitle='더 궁금한 점이 있으신가요? 아래 버튼을 눌러주세요.'
-            />
-
-            <button
-              type='button'
-              className='max-w-[35rem] flex items-center justify-center gap-[1rem] bg-yellow_kakao rounded-[2rem_2rem_2rem_0] w-full h-[6.5rem] cursor-pointer'
-            >
-              <KakaoChannelIcon size={36} />
-              <span className='h-[4rem] flex items-center justify-center bg-white_100 rounded-radius10 text-size18 font-semiBold px-[1.6rem]'>
-                <p>PINKCOTTON</p>
-                <p>고객센터</p>
-              </span>
-            </button>
-          </article>
+          <MainService />
         </div>
       </div>
     </section>
