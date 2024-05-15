1e0a6da4a9245fdb2a265834146aaa7966ca45c4
지인혁 <dhgg321@naver.com>
2024-05-15 18:23:42 +0900

    🌱feat: BestInvitations 목록 조회 기능 개발

--- src/app/(Web)/wedding/invitations/_components/BestInvitations/BestInvitations.tsx
+++ src/app/(Web)/wedding/invitations/_components/BestInvitations/BestInvitations.tsx
@@ -1,13 +1,34 @@
-import { QUERY_OPTIONS, getQueryClient } from '@/constants';
+import { CommonList, TemplateItem } from '@/components/server';
+import { QUERY_KEYS, QUERY_OPTIONS, getQueryClient } from '@/constants';
+import { BestInvitationsResponse } from '@/types/response';
 
 const BestInvitations = async () => {
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery(QUERY_OPTIONS.BEST_INVITATIONS());
-  // const { result } = queryClient.getQueryData(QUERY_KEYS.BEST_INVITATIONS);
+  const data = queryClient.getQueryData<BestInvitationsResponse>(QUERY_KEYS.BEST_INVITATIONS);
+  const { productInfoList } = data?.result || { productInfoList: [] };
 
   return (
-    <section>
-      <h3>BEST</h3>
+    <section className='py-[7.3rem]'>
+      <h3 className='text-[3rem] font-semibold px-[10%]'>BEST</h3>
+      <CommonList
+        data={productInfoList}
+        className='flex flex-wrap justify-between px-[10%]'
+      >
+        {({ templateName, newest, price, discountedPrice }) => (
+          <li key={templateName}>
+            <TemplateItem
+              name={templateName}
+              isNew={newest}
+              price={price}
+              discountPrice={discountedPrice}
+              id={templateName}
+            >
+              {templateName}
+            </TemplateItem>
+          </li>
+        )}
+      </CommonList>
     </section>
   );
 };
