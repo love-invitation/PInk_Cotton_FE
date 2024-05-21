04e3bb7ab81e28672456621e17216df21b62efb7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-21 19:13:37 +0900

    🛠️refactor: 반응형 개선

--- src/app/(Web)/_components/MainServiceSection/_components/MainQuestion/MainQuestion.tsx
+++ src/app/(Web)/_components/MainServiceSection/_components/MainQuestion/MainQuestion.tsx
@@ -6,13 +6,15 @@ import { MAIN_QUESTION_LIST } from './constants';
 
 const MainQuestion = () => {
   return (
-    <article className='flex flex-col gap-[4rem] grow-1 items-center main_page_1050:items-start'>
-      <MainSectionTitle
-        title='자주 묻는 질문'
-        subTitle='자주 들어오는 질문입니다. 더 궁금한 점이 있다면 카카오톡채널을 이용해주세요!'
-      />
+    <article className='flex flex-col gap-[4rem] grow-1 items-center'>
+      <div className='w-full px-[1rem]'>
+        <MainSectionTitle
+          title='자주 묻는 질문'
+          subTitle='자주 들어오는 질문입니다. 더 궁금한 점이 있다면 카카오톡채널을 이용해주세요!'
+        />
+      </div>
 
-      <ul className='max-w-[40rem] main_page_1050:max-w-full'>
+      <ul className='max-w-[40rem] laptop:max-w-full'>
         {MAIN_QUESTION_LIST.map(({ question, answer }) => (
           <>
             <Divider />
--- src/app/(Web)/_components/MainServiceSection/_components/MainService/MainService.tsx
+++ src/app/(Web)/_components/MainServiceSection/_components/MainService/MainService.tsx
@@ -10,18 +10,20 @@ const MainService = () => {
   const { showModal, isShowModal, closeModal } = useModal();
 
   return (
-    <article className='flex flex-col gap-[4rem] items-center main_page_1050:items-start'>
-      <MainSectionTitle
-        title='고객센터'
-        subTitle='더 궁금한 점이 있으신가요? 아래 버튼을 눌러주세요.'
-      />
+    <article className='flex flex-col gap-[4rem] items-center'>
+      <div className='w-full px-[1rem]'>
+        <MainSectionTitle
+          title='고객센터'
+          subTitle='더 궁금한 점이 있으신가요? 아래 버튼을 눌러주세요.'
+        />
+      </div>
 
       <button
         type='button'
         className='max-w-[35rem] flex items-center justify-center gap-[1rem] bg-yellow_kakao rounded-[2rem_2rem_2rem_0] w-full h-[6.5rem]'
         onClick={showModal}
       >
-        <KakaoChannelIcon size={36} />
+        <KakaoChannelIcon size='3.6rem' />
         <p className='h-[4rem] flex items-center justify-center bg-white_100 rounded-radius10 text-size18 font-semiBold px-[1.6rem]'>
           PINKCOTTON 고객센터
         </p>
