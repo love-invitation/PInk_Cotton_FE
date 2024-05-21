333bbde92d4d14630a879bb6d5f3677c781c58c0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-21 19:30:20 +0900

    🛠️refactor: 반응형 개선

--- src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
+++ src/components/server/Footer/components/FooterNavigator/FooterNavigator.tsx
@@ -6,9 +6,9 @@ const FooterNavigator = () => {
   const liStyle = 'hover:brightness-75 hover:underline transition-all';
   return (
     <nav className='w-full flex items-center pl-[2rem]'>
-      <h6 className='text-[1.2rem] font-bold mr-[2rem]'>PINKCOTTON</h6>
+      <h6 className='text-size14 font-bold mr-[2rem]'>PINKCOTTON</h6>
 
-      <ul className='flex flex-grow text-[1.2rem] h-[1.8rem] items-center text-gray_100'>
+      <ul className='flex flex-grow text-size14 h-[1.8rem] items-center text-gray_100'>
         <li className={liStyle}>
           <Link href='/'>이용약관</Link>
         </li>
