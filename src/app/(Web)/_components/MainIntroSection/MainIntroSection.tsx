7f048f6e7f9781a8d89bfae991ffccb0177e68f0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-21 17:42:18 +0900

    🛠️refactor: Main Intro Section 반응형 수정

--- src/app/(Web)/_components/MainIntroSection/MainIntroSection.tsx
+++ src/app/(Web)/_components/MainIntroSection/MainIntroSection.tsx
@@ -4,7 +4,7 @@ const MainIntroSection = () => {
   return (
     <section
       id='calc_header_height'
-      className='w-full flex justify-center items-center main_page_1050:gap-[2rem] main_page_1050:min-h-[120rem]'
+      className='w-full flex justify-center items-center laptop:gap-[2rem] laptop:min-h-[120rem]'
     >
       <MainLeftIntroduce />
       <MainRightIntroduce />
