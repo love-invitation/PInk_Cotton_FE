decd5825e799e3dd6a7529d4a1820e00315e4c8b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-23 17:45:13 +0900

    🌱feat: 반응형 개선

--- src/app/(Web)/@modal/(.)login/page.tsx
+++ src/app/(Web)/@modal/(.)login/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 
 import { LOGIN_IMAGE } from '@/assets/images';
 
+import { LoginButtons } from './_components';
 import { useCloseAwayClick } from './_hooks';
 
 const LoginModal = () => {
@@ -13,22 +14,24 @@ const LoginModal = () => {
     <div className='w-dvw h-dvh bg-transparent_90 fixed top-0 left-0 z-modal flex items-center justify-center'>
       <article
         ref={ref}
-        className='w-full max-w-[60rem] h-[60rem] py-[4rem] bg-white_100 rounded-radius25 flex flex-col items-center justify-center gap-[4rem]'
+        className='w-full px-[2rem] pb-[2rem] max-w-[70rem] h-[70rem] bg-white_100 rounded-radius25 flex flex-col items-center justify-center gap-[4rem]'
       >
         <span className='flex flex-col items-center justify-center gap-[1.6rem]'>
           <h2 className='text-size26 font-medium'>로그인</h2>
           <p className='text-size18 font-medium break-keep text-gray_100'>
-            PINK COTTON은 모바일청첩장을 가장 쉽고 빠르게 만들 수 있는 공간이에요.
+            PINK COTTON은 모바일청첩장을 가장 쉽고 빠르게 만들수 있는 공간이에요.
           </p>
         </span>
 
-        <i className='w-[25rem] h-[18rem]'>
+        <i className='max-w-[25.6rem] h-[18rem]'>
           <Image
             src={LOGIN_IMAGE}
             sizes='100%'
             alt='login_image'
           />
         </i>
+
+        <LoginButtons />
       </article>
     </div>
   );
