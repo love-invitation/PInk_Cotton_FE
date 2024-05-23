599a93f84b4899ac1a65832c39636f04daa4517d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-23 17:54:26 +0900

    🌱feat: Login Modal Animation 구현

--- src/app/(Web)/@modal/(.)login/_hooks/useCloseAwayClick/useCloseAwayClick.ts
+++ src/app/(Web)/@modal/(.)login/_hooks/useCloseAwayClick/useCloseAwayClick.ts
@@ -1,17 +1,24 @@
 'use client';
 
+import { useCallback, useState } from 'react';
+
 import { useRouter } from 'next/navigation';
 
 import useClickAway from '@/hooks/useClickAway/useClickAway';
 
 const useCloseAwayClick = () => {
   const route = useRouter();
+  const [isExit, setIsExit] = useState(false);
 
   const ref = useClickAway<HTMLElement>(() => {
-    route.back();
+    setIsExit(true);
   });
 
-  return ref;
+  const handleBackRoute = useCallback(() => {
+    route.back();
+  }, [route]);
+
+  return { ref, isExit, handleBackRoute };
 };
 
 export default useCloseAwayClick;
