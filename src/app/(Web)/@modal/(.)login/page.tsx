825be71421904d03544c567491ae2b465423626f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-23 16:42:22 +0900

    🌱feat: Login Modal Style 구현

--- src/app/(Web)/@modal/(.)login/page.tsx
+++ src/app/(Web)/@modal/(.)login/page.tsx
@@ -1,10 +1,27 @@
-'use client';
+import Image from 'next/image';
+
+import { LOGIN_IMAGE } from '@/assets/images';
 
 const LoginModal = () => {
   return (
-    <section className='w-dvw h-dvh bg-transparent_90 fixed top-0 left-0 z-modal flex items-center justify-center'>
-      <article className='w-full max-w-[60rem] h-[60rem] bg-white_100 rounded-radius25'>dd</article>
-    </section>
+    <div className='w-dvw h-dvh bg-transparent_90 fixed top-0 left-0 z-modal flex items-center justify-center'>
+      <article className='w-full max-w-[60rem] h-[60rem] py-[4rem] bg-white_100 rounded-radius25 flex flex-col items-center justify-center gap-[4rem]'>
+        <span className='flex flex-col items-center justify-center gap-[1.6rem]'>
+          <h2 className='text-size26 font-medium'>로그인</h2>
+          <p className='text-size18 font-medium break-keep text-gray_100'>
+            PINK COTTON은 모바일청첩장을 가장 쉽고 빠르게 만들 수 있는 공간이에요.
+          </p>
+        </span>
+
+        <i className='w-[25rem] h-[18rem]'>
+          <Image
+            src={LOGIN_IMAGE}
+            sizes='100%'
+            alt='login_image'
+          />
+        </i>
+      </article>
+    </div>
   );
 };
 
