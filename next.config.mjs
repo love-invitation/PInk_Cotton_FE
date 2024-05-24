9a8693fb03186cadd19616be92b5bcd452902249
wldlsgur <dhgg321@naver.com>
2024-05-24 16:19:31 +0900

    🌱feat: next/image 옵션 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'invitation-bucket.s3.ap-northeast-2.amazonaws.com',
         port: '',
         pathname: '/**',
       },
