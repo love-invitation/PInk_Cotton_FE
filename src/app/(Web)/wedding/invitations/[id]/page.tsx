531e13caeb4361665433b95873ec8cd68e2844db
지인혁 <dhgg321@naver.com>
2024-05-25 16:55:39 +0900

    🎨style: floating 스타일 수정

--- src/app/(Web)/wedding/invitations/[id]/page.tsx
+++ src/app/(Web)/wedding/invitations/[id]/page.tsx
@@ -1,4 +1,6 @@
-import Template from '@/components/server/Template/Template';
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { QUERY_KEYS, QUERY_OPTIONS, getQueryClient } from '@/constants';
 import { InvitationsResponse } from '@/types/response';
 
@@ -14,22 +16,33 @@ const Floating = async ({ params }: FloatingProps) => {
   const data = queryClient.getQueryData<InvitationsResponse>(QUERY_KEYS.ALL_INVITATIONS);
   const { title, details, groomName, brideName } = data?.result || {};
   const { productInfoList } = data?.result || { productInfoList: [] };
-  const { id, imageUrl } =
+  const { imageUrl } =
     productInfoList?.find((productInfo) => productInfo.id === Number(params.id)) || {};
 
   return (
-    <div className='h-dvh w-dvw absolute left-0 top-0 bg-black_900 text-white_100'>
-      <article>
-        <Template
-          title={title}
-          details={details || ''}
-          groomName={groomName || ''}
-          brideName={brideName || ''}
-          productItem={{
-            id: id || 0,
-            imageUrl: imageUrl || '',
-          }}
+    <div
+      className='flex justify-center items-center text-black_900'
+      id='fit-wrap'
+    >
+      <article className='flex justify-center items-center flex-col'>
+        <h3>{title}</h3>
+        <Image
+          src={imageUrl || ''}
+          alt='123'
+          width={259}
+          height={200}
         />
+        <div className='flex'>
+          <p>{groomName}</p>
+          <p>{brideName}</p>
+        </div>
+        <p>{details}</p>
+        <Link
+          href='/wedding/invitations/produce'
+          className='bg-pink_500 text-white_100'
+        >
+          제작하기
+        </Link>
       </article>
     </div>
   );
