beeb8e7d664aa365fc35dcf4ea542502db0a65cd
지인혁 <dhgg321@naver.com>
2024-05-25 16:55:39 +0900

    🎨style: floating 스타일 수정

--- src/app/(Web)/wedding/invitations/@floating/(.)[id]/page.tsx
+++ src/app/(Web)/wedding/invitations/@floating/(.)[id]/page.tsx
@@ -1,4 +1,6 @@
-import Template from '@/components/server/Template/Template';
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { QUERY_KEYS, QUERY_OPTIONS, getQueryClient } from '@/constants';
 import { InvitationsResponse } from '@/types/response';
 
@@ -14,22 +16,31 @@ const Floating = async ({ params }: FloatingProps) => {
   const data = queryClient.getQueryData<InvitationsResponse>(QUERY_KEYS.ALL_INVITATIONS);
   const { title, details, groomName, brideName } = data?.result || {};
   const { productInfoList } = data?.result || { productInfoList: [] };
-  const { id, imageUrl } =
+  const { imageUrl } =
     productInfoList?.find((productInfo) => productInfo.id === Number(params.id)) || {};
 
   return (
-    <div className='h-dvh w-dvw'>
-      <article>
-        <Template
-          title={title}
-          details={details || ''}
-          groomName={groomName || ''}
-          brideName={brideName || ''}
-          productItem={{
-            id: id || 0,
-            imageUrl: imageUrl || '',
-          }}
+    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+      <article className='flex flex-col justify-center items-center gap-[2rem] text-black_900 bg-white_100 py-[4rem]'>
+        <h3 className='text-[1rem]'>{title}</h3>
+        <Image
+          src={imageUrl || ''}
+          alt='123'
+          width={259}
+          height={200}
         />
+        <div className='flex gap-[1rem] text-[1.6rem]'>
+          <p>{groomName}</p>
+          <p>·</p>
+          <p>{brideName}</p>
+        </div>
+        <p className='text-[1rem]'>{details}</p>
+        <Link
+          href='wedding/invitations/produce'
+          className='bg-pink_500 text-white_100 w-full py-[1rem] flex justify-center'
+        >
+          제작하기
+        </Link>
       </article>
     </div>
   );
--- src/components/server/Template/Template.tsx
+++ src/components/server/Template/Template.tsx
@@ -1,32 +1,4 @@
-import Image from 'next/image';
-
-import { TemplateProps } from './Template.type';
-
-const Template = ({ title, details, groomName, brideName, productItem }: TemplateProps) => {
-  const { id, imageUrl } = productItem;
-
-  if (id === 1) {
-    return (
-      <>
-        <header>
-          <h3>{title}</h3>
-          <Image
-            src={imageUrl}
-            alt='123'
-            width={259}
-            height={200}
-          />
-          <div>
-            <p>{groomName}</p>
-            <p>{brideName}</p>
-          </div>
-
-          <p>{details}</p>
-        </header>
-      </>
-    );
-  }
-
+const Template = () => {
   return null;
 };
 
