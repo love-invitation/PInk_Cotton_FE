a1a0ded1c076ed53830ba2aabacb3958a491b57e
wldlsgur <dhgg321@naver.com>
2024-05-28 21:05:53 +0900

    🛠️refactor:  all, best 청첩장 데이터 fetch 코드 리펙토링

--- src/app/(Web)/wedding/invitations/_components/AllInvitations/AllInvitations.tsx
+++ src/app/(Web)/wedding/invitations/_components/AllInvitations/AllInvitations.tsx
@@ -1,18 +1,19 @@
-import { QUERY_KEYS, QUERY_OPTIONS, getQueryClient } from '@/constants';
+import { QUERY_KEYS, getQueryClient } from '@/constants';
 import { InvitationsResponse } from '@/types/response';
 
 import { InvitationsList } from '..';
 
-const AllInvitations = async () => {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(QUERY_OPTIONS.ALL_INVITATIONS());
-  const data = queryClient.getQueryData<InvitationsResponse>(QUERY_KEYS.ALL_INVITATIONS);
-  const { productInfoList } = data?.result || { productInfoList: [] };
+const AllInvitations = () => {
+  const data = getQueryClient().getQueryData<InvitationsResponse>(QUERY_KEYS.ALL_INVITATIONS);
+
+  if (!data) {
+    return;
+  }
 
   return (
     <section className='py-[7.3rem]'>
       <h3 className='text-[3rem] font-semibold px-[10%]'>ALL</h3>
-      <InvitationsList productInfoList={productInfoList} />
+      <InvitationsList productInfoList={data.result.productInfoList} />
     </section>
   );
 };
--- src/app/(Web)/wedding/invitations/page.tsx
+++ src/app/(Web)/wedding/invitations/page.tsx
@@ -1,8 +1,17 @@
+import { QUERY_OPTIONS, getQueryClient } from '@/constants';
+
 import { InvitationsBanner } from './_components';
 import AllInvitations from './_components/AllInvitations/AllInvitations';
 import BestInvitations from './_components/BestInvitations/BestInvitations';
 
-const InvitationsPage = () => {
+const InvitationsPage = async () => {
+  const queryClient = getQueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery(QUERY_OPTIONS.BEST_INVITATIONS()),
+    queryClient.prefetchQuery(QUERY_OPTIONS.ALL_INVITATIONS()),
+  ]);
+
   return (
     <>
       <InvitationsBanner />
