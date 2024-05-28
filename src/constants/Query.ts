c04ceac0c95bbb2fa6bc599aa01c6ed7b1ba8b26
wldlsgur <dhgg321@naver.com>
2024-05-28 21:02:01 +0900

    🐞fix: 캐싱 주기 24시간으로 수정

--- src/constants/Query.ts
+++ src/constants/Query.ts
@@ -1,6 +1,6 @@
 import { cache } from 'react';
 
-import { getAllInvitations, getBestInvitations } from '@/services/server';
+import { getAllInvitations, getBestInvitations, getInvitation } from '@/services/server';
 import { QueryClient } from '@tanstack/react-query';
 
 export const getQueryClient = cache(() => new QueryClient());
@@ -8,15 +8,26 @@ export const getQueryClient = cache(() => new QueryClient());
 export const QUERY_KEYS = {
   BEST_INVITATIONS: ['best', 'invitations'],
   ALL_INVITATIONS: ['all', 'invitations'],
+  INVITATION: (productInfoId: number | string) => ['invitation', productInfoId],
 };
 
 export const QUERY_OPTIONS = {
   BEST_INVITATIONS: () => ({
     queryKey: QUERY_KEYS.BEST_INVITATIONS,
     queryFn: () => getBestInvitations(),
+    gcTime: 86400000,
+    staleTime: 86400000,
   }),
   ALL_INVITATIONS: () => ({
     queryKey: QUERY_KEYS.ALL_INVITATIONS,
     queryFn: () => getAllInvitations(),
+    gcTime: 86400000,
+    staleTime: 86400000,
+  }),
+  INVITATION: (productInfoId: number | string) => ({
+    queryKey: QUERY_KEYS.INVITATION(productInfoId),
+    queryFn: () => getInvitation(productInfoId),
+    gcTime: 86400000,
+    staleTime: 86400000,
   }),
 };
