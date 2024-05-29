97c54a8a65776c3e40cc10d3258680d5ebbd5858
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 02:22:08 +0900

    🌱feat: Templates Type지정

---
+++ src/components/client/Templates/Templates.type.ts
@@ -0,0 +1,14 @@
+export interface TemplatesProps {
+  id: string;
+  details: string;
+  groomName: string;
+  brideName: string;
+  imageUrl: string;
+}
+
+export interface TemplateProps {
+  details: string;
+  groomName: string;
+  brideName: string;
+  imageUrl: string;
+}
