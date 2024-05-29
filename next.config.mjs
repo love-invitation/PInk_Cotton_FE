0046f1d7cb1358ab48eb112b9a03122598d174bb
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 01:39:28 +0900

    🔨chore: Image url

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'invitation-bucket.s3.ap-northeast-2.',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
