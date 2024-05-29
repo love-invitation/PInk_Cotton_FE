7ceedbd8ac1682b86b53dbf65b8b4781f7ce26d0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 02:22:49 +0900

    🌱feat: Templates 와 Template1 제작

--- src/components/client/Templates/Templates.tsx
+++ src/components/client/Templates/Templates.tsx
@@ -1,22 +1,31 @@
+import { TemplatesProps } from './Templates.type';
 import { Template1 } from './components';
 
-const Templates = ({ id }: { id: string }) => {
+const Templates = ({ id, details = '', brideName, groomName, imageUrl }: TemplatesProps) => {
+  /**
+   * 서버에서 받아온 데이터의 경우 데이터는 \n로 보이지만
+   * 실제로는 \\n으로 강제 변환되는 경우가 존재함 따라서 아래와같이 Replace를 수행함
+   */
+  const detailReplace = details.replace(/\\n | \n/g, '\n');
+
   switch (id) {
     case 'tem1':
       return (
         <Template1
-          detail='2025.10.20.토요일 오후 2시 \n 메종 드 프라이어 그랜드홀'
-          groomName='도레미'
-          brideName='파솔라'
+          details={detailReplace}
+          groomName={groomName}
+          brideName={brideName}
+          imageUrl={imageUrl}
         />
       );
 
     default:
       return (
         <Template1
-          detail='2025.10.20.토요일 오후 2시 \n 메종 드 프라이어 그랜드홀'
-          groomName='도레미'
-          brideName='파솔라'
+          details={detailReplace}
+          groomName={groomName}
+          brideName={brideName}
+          imageUrl={imageUrl}
         />
       );
   }
