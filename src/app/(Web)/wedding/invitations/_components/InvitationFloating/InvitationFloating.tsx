74f5886765dcc15420279b5b690a143fd7494392
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 02:31:48 +0900

    🛠️refactor: InvitationFloating Templates 반영

--- src/app/(Web)/wedding/invitations/_components/InvitationFloating/InvitationFloating.tsx
+++ src/app/(Web)/wedding/invitations/_components/InvitationFloating/InvitationFloating.tsx
@@ -3,9 +3,11 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { InvitationTemplate } from '@/components/server';
+import { Templates } from '@/components/client';
+import { TemplateLayout } from '@/components/server';
 import { QUERY_OPTIONS } from '@/constants';
 import useClickAway from '@/hooks/useClickAway/useClickAway';
+import { InvitationResponse } from '@/types/response';
 import { useQuery } from '@tanstack/react-query';
 
 import { InvitationFloatingProps } from './InvitationFloating.type';
@@ -13,7 +15,8 @@ import { InvitationFloatingProps } from './InvitationFloating.type';
 const InvitationFloating = ({ id }: InvitationFloatingProps) => {
   const route = useRouter();
   const ref = useClickAway<HTMLDivElement>(() => route.back());
-  const { data } = useQuery(QUERY_OPTIONS.INVITATION(id));
+
+  const { data } = useQuery<InvitationResponse>(QUERY_OPTIONS.INVITATION(id));
 
   if (!data?.result) {
     return;
@@ -24,15 +27,18 @@ const InvitationFloating = ({ id }: InvitationFloatingProps) => {
       className='flex flex-col justify-center items-center gap-[2rem]'
       ref={ref}
     >
-      <InvitationTemplate
-        title={data.result.title}
-        details={data.result.details}
-        groomName={data.result.groomName}
-        brideName={data.result.brideName}
-        productInfo={data.result.productInfo}
-        width={200}
-        height={100}
-      />
+      <TemplateLayout size='large'>
+        <Templates
+          id={
+            // 추후에 아이디값 결정 후 반영하기
+            'tem1'
+          }
+          details={data.result.details}
+          groomName={data.result.groomName}
+          brideName={data.result.brideName}
+          imageUrl={data.result.productInfo.imageUrl}
+        />
+      </TemplateLayout>
       <Link
         href={`/wedding/invitations/produce${id}`}
         className='bg-pink_500 text-white_100 w-full py-[1rem] flex justify-center text-[1.6rem] border-solid	border-2 rounded-2xl'
