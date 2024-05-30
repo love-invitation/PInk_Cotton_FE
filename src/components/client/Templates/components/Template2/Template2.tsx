2be5db37d1a29a7be8ef1f6cce07a24d95453ad0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 13:24:15 +0900

    🌱feat: Template2 컴포넌트 생성

---
+++ src/components/client/Templates/components/Template2/Template2.tsx
@@ -0,0 +1,5 @@
+const Template2 = () => {
+  return <div>Template2</div>;
+};
+
+export default Template2;
--- src/components/client/Templates/components/index.ts
+++ src/components/client/Templates/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Template1 } from './Template1/Template1';
+export { default as Template2 } from './Template2/Template2';
