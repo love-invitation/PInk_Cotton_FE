83c79454a23544bc33e771f76faf09e7d336f895
wldlsgur <dhgg321@naver.com>
2024-05-30 21:04:49 +0900

    🌱feat: 제작 form 기본 값 설정

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_constants/DefaultValue.ts
@@ -0,0 +1,60 @@
+const ACCOUNT = {
+  name: '',
+  number: '',
+};
+
+const PERSON = {
+  name: '',
+  phone: '',
+  account: { ...ACCOUNT },
+  relation: '',
+  father: { account: { ...ACCOUNT }, name: '', phone: '' },
+  mother: { account: { ...ACCOUNT }, name: '', phone: '' },
+};
+
+const IMAGE_SECTION = {
+  image: '',
+  title: '',
+  contents: '',
+};
+
+const INVITATION = {
+  cover: { ...IMAGE_SECTION },
+  thumbnail: { ...IMAGE_SECTION },
+  article: {
+    title: '',
+    contents: '',
+  },
+  reservation: {
+    date: [],
+    type: '',
+  },
+  place: {
+    name: '',
+    detail: '',
+    address: '',
+  },
+  transportation: [{ kind: '', detail: '' }],
+  gallery: [],
+  groom: { ...PERSON },
+  bride: { ...PERSON },
+  guestbookCheck: false,
+  priority: {
+    article: 0,
+    weddingDate: 0,
+    weddingPlace: 0,
+    transport: 0,
+    contact: 0,
+    thumbnail: 0,
+    gallery: 0,
+    guestbook: 0,
+    account: 0,
+  },
+};
+
+export default {
+  ACCOUNT,
+  PERSON,
+  IMAGE_SECTION,
+  INVITATION,
+};
