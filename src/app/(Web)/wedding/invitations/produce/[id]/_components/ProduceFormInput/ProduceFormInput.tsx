181c23245425059232e74945fb14c33251d9afcd
wldlsgur <dhgg321@naver.com>
2024-05-30 21:03:38 +0900

    🌱feat: 청첩장 제작 페이지 기본 레이아웃 및 hook form 설정

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
@@ -0,0 +1,5 @@
+const ProduceFormInput = () => {
+  return <section className='basis-[60%]'>제작 하자</section>;
+};
+
+export default ProduceFormInput;
---
+++ src/app/(Web)/wedding/invitations/produce/[id]/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { InvitationCover, ProduceFormInput } from './_components';
+import { DEFAULT_VALUE } from './_constants';
+
+const ProducePage = () => {
+  const form = useForm({
+    defaultValues: DEFAULT_VALUE.INVITATION,
+  });
+
+  return (
+    <FormProvider {...form}>
+      <form className='flex py-[4.8rem] px-[12rem]'>
+        <InvitationCover />
+        <ProduceFormInput />
+      </form>
+    </FormProvider>
+  );
+};
+
+export default ProducePage;
