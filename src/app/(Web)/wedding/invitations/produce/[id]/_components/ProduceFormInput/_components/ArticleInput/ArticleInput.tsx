3b6ad7b8cb67ebc8b59147627e3e110f5f1a962b
지인혁 <dhgg321@naver.com>
2024-06-02 19:21:55 +0900

    🌱feat: 청첩장 제작 ArticleInput 기능 개발

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/ArticleInput/ArticleInput.tsx
@@ -0,0 +1,27 @@
+import { Accordion, Input, TextEditor } from '@/components/client';
+
+const ArticleInput = () => {
+  return (
+    <Accordion
+      buttonTitle='모시는 글'
+      type='edit'
+      defaultToggleValue
+    >
+      <Input className='flex-col gap-[2.1rem] py-[4rem]'>
+        <div className='flex items-center gap-[3.3rem]'>
+          <Input.Label>제목</Input.Label>
+          <Input.Input
+            name='article.title'
+            placeholder='소중한 분들을 초대합니다.'
+          />
+        </div>
+        <div className='flex gap-[3.3rem]'>
+          <Input.Label>내용</Input.Label>
+          <TextEditor name='article.contents' />
+        </div>
+      </Input>
+    </Accordion>
+  );
+};
+
+export default ArticleInput;
