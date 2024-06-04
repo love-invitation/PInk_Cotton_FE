732acd834845147c58a195a98b36ab2f1c6fda89
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-04 17:12:08 +0900

    🌱feat: GroomBrideInput 생성

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/GroomBrideInput/GroomBrideInput.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { FormEvent, useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { Accordion, CheckBox, Input } from '@/components/client';
+
+import { GroomBrideInputProps } from './GroomBrideInput.type';
+
+const GroomBrideInput = ({ type }: GroomBrideInputProps) => {
+  const { watch, setValue } = useFormContext();
+
+  const handleClickCheckBox = (e: FormEvent<HTMLInputElement>, name: string) => {
+    setValue(name, e.currentTarget.checked);
+  };
+
+  const typeName = useMemo(() => (type === 'bride' ? '신부' : '신랑'), [type]);
+
+  const commonStyle = {
+    inputContainer: 'w-full flex items-center gap-[2rem]',
+    inputLabel: 'w-[5rem]',
+    input: 'w-[20rem]',
+    checkboxContainer: 'h-full flex items-center gap-2',
+    checkboxLabel: 'text-size16 font-bold',
+  };
+
+  return (
+    <Accordion
+      type='edit'
+      buttonTitle={`${typeName}측 정보`}
+      defaultToggleValue
+    >
+      <Input className='w-full flex flex-col py-[2rem] gap-[2rem]'>
+        <div className={commonStyle.inputContainer}>
+          <Input.Label className={commonStyle.inputLabel}>{typeName}</Input.Label>
+          <Input.Input
+            className={commonStyle.input}
+            name={`${type}.name`}
+            placeholder='이름'
+          />
+        </div>
+
+        <div className={commonStyle.inputContainer}>
+          <Input.Label className={commonStyle.inputLabel}>아버님</Input.Label>
+          <Input.Input
+            className={commonStyle.input}
+            name={`${type}.father.name`}
+            placeholder='이름'
+          />
+
+          <div className={commonStyle.checkboxContainer}>
+            <CheckBox
+              id={`produce_${type}_father`}
+              checked={watch(`${type}.father.deceased`)}
+              onChange={(event) => handleClickCheckBox(event, `${type}.father.deceased`)}
+            />
+            <label
+              className={commonStyle.checkboxLabel}
+              htmlFor={`produce_${type}_father`}
+            >
+              故
+            </label>
+          </div>
+        </div>
+
+        <div className={commonStyle.inputContainer}>
+          <Input.Label className={commonStyle.inputLabel}>어머님</Input.Label>
+          <Input.Input
+            className={commonStyle.input}
+            name={`${type}.mother.name`}
+            placeholder='이름'
+          />
+
+          <div className={commonStyle.checkboxContainer}>
+            <CheckBox
+              id={`produce_${type}_mother`}
+              checked={watch(`${type}.mother.deceased`)}
+              onChange={(event) => handleClickCheckBox(event, `${type}.mother.deceased`)}
+            />
+            <label
+              className={commonStyle.checkboxLabel}
+              htmlFor={`produce_${type}_mother`}
+            >
+              故
+            </label>
+          </div>
+        </div>
+
+        <div className={commonStyle.inputContainer}>
+          <Input.Label className={commonStyle.inputLabel}>관계</Input.Label>
+          <Input.Input
+            className={commonStyle.input}
+            name={`${type}.relation`}
+            placeholder={type === 'bride' ? '딸' : '아들'}
+          />
+        </div>
+
+        <p className='mt-[1rem] text-size14 text-gray_200 font-medium'>
+          아버님, 어머님 성함은 생략하실 수 있습니다.
+        </p>
+      </Input>
+    </Accordion>
+  );
+};
+
+export default GroomBrideInput;
