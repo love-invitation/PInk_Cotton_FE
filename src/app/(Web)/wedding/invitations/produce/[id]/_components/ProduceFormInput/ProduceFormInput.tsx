d501729066ec8a8dc1476387db90e05aa625fc25
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-04 17:14:20 +0900

    🌱feat: GroomBrideInput 반영 및 미사용 필드 주석처리

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
@@ -1,10 +1,12 @@
-import { ArticleInput, CoverInput } from './_components';
+import { ArticleInput, CoverInput, GroomBrideInput } from './_components';
 
 const ProduceFormInput = () => {
   return (
     <section className='flex flex-col gap-[2rem]'>
       <CoverInput />
       <ArticleInput />
+      <GroomBrideInput type='groom' />
+      <GroomBrideInput type='bride' />
     </section>
   );
 };
--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/index.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/index.tsx
@@ -1,2 +1,3 @@
 export { default as CoverInput } from './CoverInput/CoverInput';
 export { default as ArticleInput } from './ArticleInput/ArticleInput';
+export { default as GroomBrideInput } from './GroomBrideInput/GroomBrideInput';
