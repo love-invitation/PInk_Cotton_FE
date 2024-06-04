860bd33c1cd57fd2f442caa7a431bedf258d34f6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-04 20:48:52 +0900

    🌱feat: 주소 API 실패에 대한 AlertModal 구현

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
@@ -3,11 +3,11 @@ import { ArticleInput, CoverInput, GroomBrideInput, WeddingPlageInput } from './
 const ProduceFormInput = () => {
   return (
     <section className='w-[60%] min-w-[30rem] flex flex-col gap-[2rem] mobile:w-full'>
-      <WeddingPlageInput />
       <CoverInput />
       <ArticleInput />
       <GroomBrideInput type='groom' />
       <GroomBrideInput type='bride' />
+      <WeddingPlageInput />
     </section>
   );
 };
---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/WeddingPlageInput/hooks/useGetAddress/useGetAddress.type.ts
@@ -0,0 +1,3 @@
+export interface UseGetAddressProps {
+  showModal: () => void;
+}
