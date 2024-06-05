99823fbd1476aea95979a73f687ff6ffc1ac85ba
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-05 22:57:33 +0900

    🌱feat: InvitationCover 구현

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCover/InvitationCover.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCover/InvitationCover.tsx
@@ -1,5 +1,35 @@
+import { useFormContext } from 'react-hook-form';
+
+import { useParams } from 'next/navigation';
+
+import { WeddingTemplates } from '@/components/client';
+import { useGetElementWidth } from '@/hooks';
+
 const InvitationCover = () => {
-  return <section className='min-w-[20rem] w-[40%] border-2'>표지 사진</section>;
+  const { watch } = useFormContext();
+  const { ref, width } = useGetElementWidth();
+  const { id } = useParams();
+
+  const IMAGE_ASSETS =
+    'https://invitation-bucket.s3.ap-northeast-2.amazonaws.com/productInfo/sample_image.png';
+
+  return (
+    <section className='min-w-[20rem] w-[40%] mobile:w-full'>
+      <article
+        ref={ref}
+        className='w-full h-[20rem] shadow-shadow_500'
+        style={{ height: `${width * 1.76}px`, borderRadius: `${width * 0.0702}px` }}
+      >
+        <WeddingTemplates
+          id={id}
+          details={watch('cover.contents')}
+          groomName={watch('groom.name')}
+          brideName={watch('bride.name')}
+          imageUrl={watch('cover.image') || IMAGE_ASSETS}
+        />
+      </article>
+    </section>
+  );
 };
 
 export default InvitationCover;
