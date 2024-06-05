f5ac07ce3ea7f4aa01ef5f79ed3255e49d256ea0
지인혁 <dhgg321@naver.com>
2024-06-06 00:56:48 +0900

    🌱feat: 청첩장 제작 페이지 예약 일시 input 기능 개발

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/ReservationInput/ReservationInput.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+
+import { Accordion, CheckBox, Input } from '@/components/client';
+
+const ReservationInput = () => {
+  const { watch, setValue } = useFormContext();
+
+  return (
+    <Accordion
+      buttonTitle='예약일시'
+      type='edit'
+      defaultToggleValue
+    >
+      <div className='flex flex-col gap-[1.9rem] py-[4rem]'>
+        <Input className='items-center gap-[3.3rem]'>
+          <Input.Label>예약일</Input.Label>
+          <Input.Input
+            type='date'
+            name='reservation.date'
+            className=''
+          />
+        </Input>
+        <Input className='items-center gap-[4.7rem]'>
+          <Input.Label>시각</Input.Label>
+          <Input.Input
+            type='time'
+            name='reservation.time'
+          />
+        </Input>
+        <Input className='items-center gap-[4.7rem]'>
+          <Input.Label>표시</Input.Label>
+          <div className='flex items-center gap-[1rem]'>
+            <CheckBox
+              id='mark'
+              checked={watch(`reservation.type`)}
+              onChange={(event) => setValue('reservation.type', event.currentTarget.checked)}
+            />
+            <label
+              htmlFor='mark'
+              className='text-[1.7rem] text-black_800 font-semibold'
+            >
+              달력
+            </label>
+          </div>
+        </Input>
+      </div>
+    </Accordion>
+  );
+};
+
+export default ReservationInput;
