e0d8cc2451149d0fb14707b44481a972ecea3545
지인혁 <dhgg321@naver.com>
2024-06-06 00:53:29 +0900

    🛠️refactor: Input 공용 컴포넌트 type 수정

--- src/components/client/Input/Input/Input.type.ts
+++ src/components/client/Input/Input/Input.type.ts
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes } from 'react';
 import { RegisterOptions } from 'react-hook-form';
 
-export type InputType = 'text' | 'email' | 'number' | 'password' | 'tel';
+export type InputType = 'text' | 'email' | 'number' | 'password' | 'tel' | 'date' | 'time';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: InputType;
