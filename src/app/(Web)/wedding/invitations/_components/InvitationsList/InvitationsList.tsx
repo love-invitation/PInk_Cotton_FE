35c5189713347683779a26de5b5beccfbfb88b82
지인혁 <dhgg321@naver.com>
2024-06-07 23:31:57 +0900

    🎨style: 청첩장 종류 페이지 높이 넘치는 문제 해결

--- src/app/(Web)/wedding/invitations/_components/InvitationsList/InvitationsList.tsx
+++ src/app/(Web)/wedding/invitations/_components/InvitationsList/InvitationsList.tsx
@@ -8,7 +8,7 @@ const InvitationsList = ({ productInfoList }: InvitationsListProps) => {
   return (
     <CommonList
       data={productInfoList}
-      className='grid grid-cols-auto-fill-minmax auto-rows-auto gap-[10%] px-[10%]'
+      className='grid grid-cols-auto-fill-minmax auto-rows-auto gap-[12rem] px-[10%]'
     >
       {({ id, templateName, newest, price, discountedPrice }) => (
         <li
