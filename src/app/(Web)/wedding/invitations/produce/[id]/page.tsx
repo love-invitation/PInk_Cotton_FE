8d2e8d547336b930fead1d73eae4df8ab9d07088
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-08 16:14:12 +0900

    🎨style: Produce 전체적인 반응형 레이아웃 개선

--- src/app/(Web)/wedding/invitations/produce/[id]/page.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/page.tsx
@@ -14,10 +14,12 @@ const ProducePage = () => {
     <FormProvider {...form}>
       <form
         id='calc_header_footer_height'
-        className='flex w-full py-[4.8rem] px-[10rem] tablet:px-[5rem] mobile:px-[2rem] gap-[2rem] justify-center mobile:flex-col'
+        className='flex w-full py-[4.8rem] px-[10rem] tablet:px-[5rem] mobile:px-[2rem] gap-[2rem] justify-center'
       >
-        <InvitationCover />
-        <ProduceFormInput />
+        <div className='w-full max-w-[140rem] flex tablet:gap-[2rem] gap-[8rem] justify-center mobile:flex-col'>
+          <InvitationCover />
+          <ProduceFormInput />
+        </div>
       </form>
     </FormProvider>
   );
