7a33681618bbb260e5d9ee15f9c721df81da69d4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-08 16:49:05 +0900

    📝change: Input 순서 변경

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
@@ -11,9 +11,9 @@ const ProduceFormInput = () => {
   return (
     <section className='w-[60%] min-w-[30rem] flex flex-col gap-[2rem] mobile:w-full'>
       <CoverInput />
-      <ArticleInput />
       <GroomBrideInput type='groom' />
       <GroomBrideInput type='bride' />
+      <ArticleInput />
       <ReservationInput />
       <WeddingPlaceInput />
       <TransportationInput />
