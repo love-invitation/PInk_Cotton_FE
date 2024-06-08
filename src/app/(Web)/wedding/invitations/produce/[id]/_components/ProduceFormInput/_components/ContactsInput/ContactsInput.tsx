bf0ea702e61c56dcc210a35c0c2aa858c9378252
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-08 18:36:53 +0900

    🌱feat: ContactsInput 구현

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/ContactsInput/ContactsInput.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/ContactsInput/ContactsInput.tsx
@@ -1,5 +1,20 @@
+import { Accordion, Input } from '@/components/client';
+
+import { ContactInputItem } from './components';
+
 const ContactsInput = () => {
-  return <div>ContactsInput</div>;
+  return (
+    <Accordion
+      type='edit'
+      buttonTitle='연락처'
+      defaultToggleValue
+    >
+      <Input className='w-full flex flex-col'>
+        <ContactInputItem type='groom' />
+        <ContactInputItem type='bride' />
+      </Input>
+    </Accordion>
+  );
 };
 
 export default ContactsInput;
