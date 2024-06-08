13d2375d588ce493ff890e2fbc152bc3910493d0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-08 16:13:50 +0900

    🌱feat: TransportationInput 컴포넌트 생성

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/ProduceFormInput.tsx
@@ -3,12 +3,14 @@ import {
   CoverInput,
   GroomBrideInput,
   ReservationInput,
+  TransportationInput,
   WeddingPlageInput,
 } from './_components';
 
 const ProduceFormInput = () => {
   return (
     <section className='w-[60%] min-w-[30rem] flex flex-col gap-[2rem] mobile:w-full'>
+      <TransportationInput />
       <CoverInput />
       <ArticleInput />
       <GroomBrideInput type='groom' />
--- src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/index.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/index.tsx
@@ -3,3 +3,4 @@ export { default as ArticleInput } from './ArticleInput/ArticleInput';
 export { default as GroomBrideInput } from './GroomBrideInput/GroomBrideInput';
 export { default as WeddingPlageInput } from './WeddingPlageInput/WeddingPlageInput';
 export { default as ReservationInput } from './ReservationInput/ReservationInput';
+export { default as TransportationInput } from './TransportationInput/TransportationInput';
