1bc9c30311061e7d30725cf0eba16f365458be69
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-09 23:29:03 +0900

    🌱feat: InvitationCoverPreview ImageIcon 적용

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCoverPreview/InvitationCoverPreview.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCoverPreview/InvitationCoverPreview.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components/client';
+import { ImageIcon } from '@/components/server';
 import { useModal } from '@/hooks';
 
 import { PreviewModal } from './components';
@@ -11,10 +12,13 @@ const InvitationCoverPreview = () => {
     <>
       <Button
         radius='rounded'
-        className='w-[8rem] h-[8rem] sticky bottom-[2rem] left-[2rem] hidden mobile:block'
+        className='w-[6rem] h-[6rem] items-center justify-center sticky bottom-[2rem] left-[2rem] hidden mobile:flex'
         onClick={showModal}
       >
-        미리보기
+        <ImageIcon
+          size='3.6rem'
+          fill='white'
+        />
       </Button>
 
       <PreviewModal
