a0b989dd34fed4e5fe322030760b29fff7075112
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-09 23:12:44 +0900

    🌱feat: PreviewModal 구현

--- src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCoverPreview/components/PreviewModal/PreviewModal.tsx
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/InvitationCoverPreview/components/PreviewModal/PreviewModal.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+
+import { useParams } from 'next/navigation';
+
+import { Button, WeddingTemplates } from '@/components/client';
 import { Modal } from '@/components/client/Modal';
 
 import { PreviewModalProps } from './PreviewModal.type';
 
+const IMAGE_ASSETS =
+  'https://invitation-bucket.s3.ap-northeast-2.amazonaws.com/productInfo/sample_image.png';
+
 const PreviewModal = ({ isShow, onClose }: PreviewModalProps) => {
+  const { id } = useParams();
+  const { watch } = useFormContext();
   return (
     <Modal
       isShow={isShow}
@@ -10,7 +22,17 @@ const PreviewModal = ({ isShow, onClose }: PreviewModalProps) => {
       clickAwayEnable
       disabledCloseButton
     >
-      PreviewModal
+      <div className='w-[26rem] h-[45.7rem]'>
+        <WeddingTemplates
+          id={typeof id === 'string' ? id : id[0]}
+          details={watch('cover.contents')}
+          groomName={watch('groom.name')}
+          brideName={watch('bride.name')}
+          imageUrl={watch('cover.image') || IMAGE_ASSETS}
+        />
+      </div>
+
+      <Button onClick={onClose}> 닫기</Button>
     </Modal>
   );
 };
