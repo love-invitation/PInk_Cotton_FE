5f378c2d2eec9fe47a076b42d883bd473df11d2d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-11 19:10:46 +0900

    🌱feat: AccountInput 컴포넌트 생성

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/AccountInput/AccountInput.tsx
@@ -0,0 +1,15 @@
+import { Accordion } from '@/components/client';
+
+const AccountInput = () => {
+  return (
+    <Accordion
+      buttonTitle='축의금 계좌'
+      type='edit'
+      defaultToggleValue
+    >
+      AccountInput
+    </Accordion>
+  );
+};
+
+export default AccountInput;
