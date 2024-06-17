4f813e20bf729aca57af1c460843f0d6676550ab
지인혁 <dhgg321@naver.com>
2024-06-17 16:41:01 +0900

    🌱feat: 방명록 인풋 기능 개발

---
+++ src/app/(Web)/wedding/invitations/produce/[id]/_components/ProduceFormInput/_components/GuestBookCheck/GuestBookCheck.tsx
@@ -0,0 +1,22 @@
+import { useFormContext } from 'react-hook-form';
+
+import { CheckBox } from '@/components/client';
+
+const GuestBookCheck = () => {
+  const { watch, setValue } = useFormContext();
+
+  return (
+    <article className='w-full overflow-hidden max-w-[73rem] rounded-radius10 shadow-shadow_500'>
+      <button
+        type='button'
+        onClick={() => setValue('guestbookCheck', !watch('guestbookCheck'))}
+        className='flex flex-1 gap-2 items-center w-full select-none break-keep text-start h-[6rem] px-[2.4rem] text-size15 font-semibold'
+      >
+        <CheckBox checked={watch('guestbookCheck')} />
+        <p>방명록</p>
+      </button>
+    </article>
+  );
+};
+
+export default GuestBookCheck;
