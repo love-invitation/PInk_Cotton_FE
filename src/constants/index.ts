0322d95c4c3ec8c366bb83f7c4fb8d352583ba21
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-20 18:00:03 +0900

    🗑️remove: 기존 TemplateTemporary 상수 제거

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,3 @@ export * from './Score';
 export * from './Company';
 export { default as DOMAIN_URL } from './DomainUrl';
 export * from './Query';
-export { default as TEMPLATE_TEMPORARY_VALUE } from './TemplateTemporary';
