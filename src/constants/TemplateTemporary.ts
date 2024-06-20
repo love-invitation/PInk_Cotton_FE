fb43f57a50d816ce6128449c8591832842a63869
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-20 16:31:02 +0900

    🛠️refactor: TemplateTemporary constants 생성

---
+++ src/constants/TemplateTemporary.ts
@@ -0,0 +1,7 @@
+const TEMPLATE_TEMPORARY_VALUE = {
+  groom: '도레미',
+  bride: '파솔라',
+  detail: '2025.10.20. 토요일 오후 2시 \n 메종 드 프라이어 그랜드홀',
+};
+
+export default TEMPLATE_TEMPORARY_VALUE;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from './Score';
 export * from './Company';
 export { default as DOMAIN_URL } from './DomainUrl';
 export * from './Query';
+export { default as TEMPLATE_TEMPORARY_VALUE } from './TemplateTemporary';
