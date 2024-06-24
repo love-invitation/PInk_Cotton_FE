9c5809b9564e4ea43e9666177caf3a211dbef17b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-24 18:03:12 +0900

    🌱feat: Invitation Produce API 테스트

--- src/app/(Product)/wedding/invite/[id]/page.tsx
+++ src/app/(Product)/wedding/invite/[id]/page.tsx
@@ -1,4 +1,13 @@
-const InvitePage = () => {
+import { QUERY_OPTIONS, getQueryClient } from '@/constants';
+
+const InvitePage = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(QUERY_OPTIONS.INVITATION_PRODUCE('592999830142739431'));
+
+  // const data = queryClient.getQueryData(QUERY_KEYS.INVITATION_PRODUCE('592999830142739431'));
+
+  // console.log(data);
+
   return <div>invite Page</div>;
 };
 
