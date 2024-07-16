f0e25e72223aad4bc70a1c515e12b0e3802f822f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-17 01:18:00 +0900

    🎨style: fadeIn 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -122,6 +122,17 @@ const config = {
       gridAutoRows: {
         auto: 'minmax(auto, auto)',
       },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+
+      animation: {
+        fadeIn: 'fadeIn 2s 0.5s ease-in-out forwards',
+      },
     },
   },
 
