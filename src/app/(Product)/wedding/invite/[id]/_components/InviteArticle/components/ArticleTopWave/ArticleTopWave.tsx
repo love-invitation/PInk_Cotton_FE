799e29183911aa88be11bf37f02d7011e21bd746
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-18 14:08:22 +0900

    🌱feat: Wave Decoration Component 구현

---
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteArticle/components/ArticleTopWave/ArticleTopWave.tsx
@@ -0,0 +1,11 @@
+const ArticleTopWave = () => {
+  return (
+    <>
+      <div className='w-[120rem] h-[120rem] bg-pink_500 absolute bottom-[98%] rounded-[40%] opacity-30 animate-wave1' />
+      <div className='w-[120rem] h-[120rem] bg-pink_500 absolute bottom-[98%] rounded-[40%] opacity-30 animate-wave2 rotate-[25deg]' />
+      <div className='w-[120rem] h-[120rem] bg-pink_500 absolute bottom-[98%] rounded-[40%] opacity-30 animate-wave3 rotate-[50deg]' />
+    </>
+  );
+};
+
+export default ArticleTopWave;
