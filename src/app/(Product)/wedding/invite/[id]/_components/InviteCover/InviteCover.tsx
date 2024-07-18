2ea67e6ad63dc11ecfb9474dbe3a3661ee071152
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-18 13:25:00 +0900

    🎨style: InviteCover 기본 사이즈 조정

--- src/app/(Product)/wedding/invite/[id]/_components/InviteCover/InviteCover.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteCover/InviteCover.tsx
@@ -4,7 +4,7 @@ import { InviteCoverProps } from './InviteCover.type';
 
 const InviteCover = ({ coverData }: InviteCoverProps) => {
   return (
-    <article className='w-full animate-fadeIn opacity-0'>
+    <article className='w-full min-h-[100dvh] flex justify-center items-center animate-fadeIn opacity-0'>
       <WeddingTemplates
         id={coverData.templateId}
         details={coverData.coverContents}
