cc30b50ba38462ad8ee383039e4750b2406e5124
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-18 14:21:27 +0900

    🌱feat: Wave 반영

--- src/app/(Product)/wedding/invite/[id]/_components/InviteArticle/InviteArticle.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteArticle/InviteArticle.tsx
@@ -3,14 +3,16 @@
 import { DarkFlowerIcon, DotIcon } from '@/components/server';
 
 import { InviteArticleProps } from './InviteArticle.type';
+import { ArticleBottomWave, ArticleTopWave } from './components';
 
 const InviteArticle = ({ article }: InviteArticleProps) => {
   const contentReplaced = article.contents.replace(/\\n | \n/g, '\n');
 
   const { groom, bride } = article;
   return (
-    <article className='w-full py-[16rem] px-[1.6rem] flex flex-col gap-[4.5rem] border-2 border-red-400 relative overflow-scroll'>
-      {/* title */}
+    <article className='w-full py-[16rem] px-[1.6rem] flex flex-col items-center gap-[4.5rem] relative overflow-hidden'>
+      <ArticleTopWave />
+
       <span className='w-full flex flex-col text-pink_500 gap-[1rem]'>
         <h2 className='w-full flex justify-center text-size18 tracking-[0.6rem] text-center'>
           INVITATION
@@ -20,32 +22,33 @@ const InviteArticle = ({ article }: InviteArticleProps) => {
         </h1>
       </span>
 
-      {/* content */}
       <p className='w-full text-size18 text-center whitespace-pre-line leading-[4rem]'>
         {contentReplaced}
       </p>
 
       <span className='w-full flex flex-col gap-[1rem]'>
-        <span className='w-full flex justify-center items-center gap-[0.4rem] text-size20'>
+        <span className='w-full flex justify-center items-center gap-[0.5rem] text-size20'>
           {!groom.father.deceased && <DarkFlowerIcon size='1.8rem' />}
           <p>{groom.father.name}</p>
           <DotIcon />
           {!groom.mother.deceased && <DarkFlowerIcon size='1.8rem' />}
           <p>{groom.mother.name}</p>
-          <small className='text-18'>의 장남</small>
+          <small className='text-18 text-nowrap'>의 장남</small>
           <p>{groom.name}</p>
         </span>
 
-        <span className='w-full flex justify-center items-center gap-[0.4rem] text-size20'>
+        <span className='w-full flex justify-center items-center gap-[0.5rem] text-size20'>
           {!bride.father.deceased && <DarkFlowerIcon size='1.8rem' />}
           <p>{bride.father.name}</p>
           <DotIcon />
           {!bride.mother.deceased && <DarkFlowerIcon size='1.8rem' />}
           <p>{bride.mother.name}</p>
-          <small className='text-18'>의 차녀</small>
+          <small className='text-18 text-nowrap'>의 차녀</small>
           <p>{bride.name}</p>
         </span>
       </span>
+
+      <ArticleBottomWave />
     </article>
   );
 };
