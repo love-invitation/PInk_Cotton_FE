f6709bc1f6f01136b00fb5c447eb3e786686ba5f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-19 17:45:04 +0900

    🌱feat: snowfall 적용

--- src/app/(Product)/wedding/invite/[id]/_components/InviteCover/InviteCover.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteCover/InviteCover.tsx
@@ -1,10 +1,19 @@
+'use client';
+
+import { Snowfall } from 'react-snowfall';
+
+import tailwindConfig from '@/../tailwind.config';
 import { WeddingTemplates } from '@/components/client';
 
 import InviteBottomWave from '../InviteBottomWave/InviteBottomWave';
 import InviteTopWave from '../InviteTopWave/InviteTopWave';
 import { InviteCoverProps } from './InviteCover.type';
 
+import resolveConfig from 'tailwindcss/resolveConfig';
+
 const InviteCover = ({ coverData }: InviteCoverProps) => {
+  const { theme } = resolveConfig(tailwindConfig);
+
   return (
     <article className='w-full min-h-[100dvh] flex justify-center items-center animate-fadeIn opacity-0 relative overflow-hidden'>
       <InviteTopWave />
@@ -16,6 +25,14 @@ const InviteCover = ({ coverData }: InviteCoverProps) => {
         imageUrl={coverData.imageUrl}
       />
       <InviteBottomWave />
+
+      <Snowfall
+        color={theme.colors.pink_300}
+        snowflakeCount={100}
+        style={{
+          opacity: 0.5,
+        }}
+      />
     </article>
   );
 };
