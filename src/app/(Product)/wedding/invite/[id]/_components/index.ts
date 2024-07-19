b464e27e29f7ded8ddfa11e528d2e09c0911a3c8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-19 19:06:49 +0900

    🌱feat: InviteContact 컴포넌트 생성

--- src/app/(Product)/wedding/invite/[id]/_components/index.ts
+++ src/app/(Product)/wedding/invite/[id]/_components/index.ts
@@ -2,3 +2,4 @@ export { default as InviteCover } from './InviteCover/InviteCover';
 export { default as InviteArticle } from './InviteArticle/InviteArticle';
 export { default as InviteTopWave } from './InviteTopWave/InviteTopWave';
 export { default as InviteBottomWave } from './InviteBottomWave/InviteBottomWave';
+export { default as InviteContact } from './InviteContact/InviteContact';
