7e5b8aab3f34bb216e2b7cbfd41eadd4d8696948
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-19 21:15:29 +0900

    🌱feat: ContactList 구현

---
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteContact/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ContactList } from './ContactList/ContactList';
+export { default as ContactItem } from './ContactItem/ContactItem';
+export { default as ContactAnchorBox } from './ContactAnchorBox/ContactAnchorBox';
