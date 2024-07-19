81839d173aeb03f0bc9cc46c47fa452643760392
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-19 21:15:56 +0900

    🌱feat: InviteContact 구현

--- src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
@@ -1,5 +1,12 @@
+import { ContactList } from './components';
+
 const InviteContact = () => {
-  return <div>InviteContact</div>;
+  return (
+    <article className='w-full py-[12rem] flex justify-around bg-[#f9f9f9] shadow-shadow_500 border-t-[0.1rem] border-b-[0.1rem]'>
+      <ContactList type='groom' />
+      <ContactList type='bride' />
+    </article>
+  );
 };
 
 export default InviteContact;
--- src/app/(Product)/wedding/invite/[id]/page.tsx
+++ src/app/(Product)/wedding/invite/[id]/page.tsx
@@ -2,7 +2,7 @@ import { QUERY_KEYS, QUERY_OPTIONS, getQueryClient } from '@/constants';
 import { InvitationResponse } from '@/types/response';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 
-import { InviteArticle, InviteCover } from './_components';
+import { InviteArticle, InviteContact, InviteCover } from './_components';
 
 // 600246308994180233 ID key 값
 
@@ -19,9 +19,10 @@ const InvitePage = async ({ params }: { params: { id: string } }) => {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <section className='w-full h-full flex justify-center bg-gray_900'>
-        <div className='max-w-[45rem] w-full h-full overflow-scroll bg-white scrollbar-hide'>
+        <div className='max-w-[45rem] w-full h-full overflow-scroll bg-white scrollbar-hide shadow-shadow_500'>
           <InviteCover coverData={data.result.cover} />
           <InviteArticle article={data.result.article} />
+          <InviteContact />
         </div>
       </section>
     </HydrationBoundary>
