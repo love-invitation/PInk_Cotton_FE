343e4f30e4bc32e83187139ad218aa5fa0bb0930
wldlsgur <dhgg321@naver.com>
2024-07-30 23:26:10 +0900

    🌱feat: TanstackProvider mutation 401 전역 처리

--- src/app/_components/TanstackProvider.tsx
+++ src/app/_components/TanstackProvider.tsx
@@ -2,10 +2,14 @@
 
 import { PropsWithChildren, useState } from 'react';
 
+import { useRouter } from 'next/navigation';
+
+import { DOMAIN_URL, QUERY_KEYS, getQueryClient } from '@/constants';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const TanstackProvider = ({ children }: PropsWithChildren) => {
+  const route = useRouter();
   const [queryClient] = useState(
     new QueryClient({
       defaultOptions: {
@@ -13,6 +17,15 @@ const TanstackProvider = ({ children }: PropsWithChildren) => {
           retry: false,
           refetchOnWindowFocus: false,
           staleTime: 60 * 1000,
+          retryOnMount: false,
+        },
+        mutations: {
+          onError(error: unknown) {
+            if (error instanceof Error && error.toString().endsWith('401')) {
+              route.push(DOMAIN_URL.LOGIN);
+              getQueryClient().invalidateQueries({ queryKey: QUERY_KEYS.AUTH_USER });
+            }
+          },
         },
       },
     }),
