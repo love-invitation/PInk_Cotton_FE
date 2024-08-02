3f05b4b0ec167b0d6cfe082ed0364600f7ce7776
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-02 17:59:46 +0900

    🌱feat: InviteCalender 컴포넌트 생성

---
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteCalender/InviteCalender.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const InviteCalender = () => {
+  return <></>;
+};
+
+export default InviteCalender;
--- src/app/(Product)/wedding/invite/[id]/_components/index.ts
+++ src/app/(Product)/wedding/invite/[id]/_components/index.ts
@@ -3,3 +3,4 @@ export { default as InviteArticle } from './InviteArticle/InviteArticle';
 export { default as InviteTopWave } from './InviteTopWave/InviteTopWave';
 export { default as InviteBottomWave } from './InviteBottomWave/InviteBottomWave';
 export { default as InviteContact } from './InviteContact/InviteContact';
+export { default as InviteCalender } from './InviteCalender/InviteCalender';
