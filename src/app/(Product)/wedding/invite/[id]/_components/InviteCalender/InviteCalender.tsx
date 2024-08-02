bde4cd1a6277c60b52a69714e2d50bf1612d9a2b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-02 18:13:24 +0900

    🎨style: calender 1차 스타일링

--- src/app/(Product)/wedding/invite/[id]/_components/InviteCalender/InviteCalender.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteCalender/InviteCalender.tsx
@@ -1,7 +1,52 @@
 'use client';
 
+import { useRef } from 'react';
+
+import { motion, useInView } from 'framer-motion';
+
 const InviteCalender = () => {
-  return <></>;
+  const ref = useRef(null);
+  const inView = useInView(ref);
+
+  const initAni = {
+    bottom: '2.4rem',
+    opacity: 0,
+  };
+
+  const animate = {
+    bottom: '0',
+    opacity: 1,
+  };
+
+  return (
+    <article
+      ref={ref}
+      className='w-full flex flex-col gap-[6rem] py-[12rem]'
+    >
+      {inView && (
+        <>
+          <motion.h2
+            className='w-full relative flex justify-center text-size16 text-pink_500 tracking-[0.4rem] text-center'
+            initial={initAni}
+            animate={animate}
+            transition={{ duration: 1 }}
+          >
+            WEDDING DAY
+          </motion.h2>
+
+          <motion.span
+            className='w-full relative flex flex-col gap-[1.2rem] items-center text-size20'
+            initial={initAni}
+            animate={animate}
+            transition={{ duration: 1, delay: 0.3 }}
+          >
+            <p>2024.04.24</p>
+            <p className='text-size18'>토요일 오후 1시 30분</p>
+          </motion.span>
+        </>
+      )}
+    </article>
+  );
 };
 
 export default InviteCalender;
