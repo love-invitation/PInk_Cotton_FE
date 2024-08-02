f6bcb9c588db10744e460e490f1c8aa2b73ec66c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-02 17:28:21 +0900

    🛠️refactor: 디자인 및 형식 변경

--- src/app/(Product)/wedding/invite/[id]/_components/InviteContact/components/ContactList/ContactList.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteContact/components/ContactList/ContactList.tsx
@@ -5,10 +5,10 @@ import ContactItem from '../ContactItem/ContactItem';
 import { ContactListProps } from './ContactList.type';
 
 const ContactList = ({ type, data }: ContactListProps) => {
-  const typeName = type === 'bride' ? '신부' : '산렁';
+  const typeName = type === 'bride' ? '신부' : '산랑';
 
   const mainData = useMemo(
-    () => data.find(({ relation }) => relation === typeName) || { phoneNumber: 'no have number' },
+    () => data.find(({ relation }) => relation === typeName),
     [data, typeName],
   );
 
@@ -19,16 +19,18 @@ const ContactList = ({ type, data }: ContactListProps) => {
 
   return (
     <ul className='w-[40%] max-w-[16rem] flex flex-col gap-[6rem]'>
-      <li className='w-full flex flex-col gap-[2rem]'>
-        <p className='w-full text-size20 text-nowrap text-center font-bold'>
-          {`${typeName}에게 연락하기`}
-        </p>
+      <p className='w-full text-size20 text-nowrap text-center font-bold'>{`${typeName}측`}</p>
 
-        <ContactAnchorBox
-          labelName={typeName}
-          phoneNumber={mainData.phoneNumber}
-        />
-      </li>
+      {mainData && (
+        <li className='w-full flex flex-col gap-[2rem]'>
+          <p className='w-full text-size16 text-gray_400 text-center'>{`${typeName}에게 연락하기`}</p>
+
+          <ContactAnchorBox
+            labelName={typeName}
+            phoneNumber={mainData.phoneNumber}
+          />
+        </li>
+      )}
 
       {otherList.length !== 0 && (
         <p className='w-full text-size16 text-gray_400 text-center'>{`${typeName} 측 혼주`}</p>
