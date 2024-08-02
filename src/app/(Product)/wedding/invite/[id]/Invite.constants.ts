4a42464fe00ca6e34d56991529965db7874997b0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-02 19:05:30 +0900

    🛠️refactor: Animation 별도 상수화

---
+++ src/app/(Product)/wedding/invite/[id]/Invite.constants.ts
@@ -0,0 +1,11 @@
+export const INVITE_ANIMATION = {
+  INIT: {
+    bottom: '2.4rem',
+    opacity: 0,
+  },
+
+  ANIMATE: {
+    bottom: '0',
+    opacity: 1,
+  },
+};
--- src/app/(Product)/wedding/invite/[id]/_components/InviteArticle/InviteArticle.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteArticle/InviteArticle.tsx
@@ -2,6 +2,7 @@
 
 import { useRef } from 'react';
 
+import { INVITE_ANIMATION } from '../../Invite.constants';
 import { InviteArticleProps } from './InviteArticle.type';
 import { ArticleGroomBrideInfo } from './components';
 
@@ -17,32 +18,30 @@ const InviteArticle = ({ article }: InviteArticleProps) => {
     <article className='w-full h-fit py-[16rem] px-[1.6rem] flex flex-col items-center gap-[4.5rem] relative overflow-hidden'>
       <motion.span
         ref={ref}
-        className='w-full flex flex-col text-pink_500 gap-[1rem] opacity-0'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: inView ? 1 : 0 }}
+        className='w-full relative flex flex-col text-pink_500 gap-[1rem] opacity-0'
+        initial={INVITE_ANIMATION.INIT}
+        animate={inView ? INVITE_ANIMATION.ANIMATE : {}}
         transition={{ duration: 1.5 }}
       >
-        <h2 className='w-full flex justify-center text-size18 tracking-[0.6rem] text-center'>
+        <h2 className='w-full flex justify-center text-size16 tracking-[0.4rem] text-center'>
           INVITATION
         </h2>
         <h1 className='w-full flex justify-center text-size22 break-keep text-center'>
           {article.title}
         </h1>
       </motion.span>
-
       <motion.p
-        className='w-full text-size18 text-center whitespace-pre-line leading-[3.2rem]'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: inView ? 1 : 0 }}
+        className='w-full relative text-size18 text-center whitespace-pre-line leading-[3.2rem]'
+        initial={INVITE_ANIMATION.INIT}
+        animate={inView ? INVITE_ANIMATION.ANIMATE : {}}
         transition={{ duration: 1.5, delay: 0.6 }}
       >
         {contentReplaced}
       </motion.p>
-
       <motion.span
-        className='w-full flex flex-col gap-[1rem]'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: inView ? 1 : 0 }}
+        className='w-full relative flex flex-col gap-[1rem]'
+        initial={INVITE_ANIMATION.INIT}
+        animate={inView ? INVITE_ANIMATION.ANIMATE : {}}
         transition={{ duration: 1.5, delay: 1.2 }}
       >
         <ArticleGroomBrideInfo info={groom} />
