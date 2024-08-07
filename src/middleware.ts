44445800f163204566280740b06595bd95da448d
wldlsgur <dhgg321@naver.com>
2024-08-07 20:33:22 +0900

    Hotfix: middleware 버그 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,23 @@
 import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { DOMAIN_URL } from './constants';
-
 export const middleware = async (request: NextRequest) => {
   const { pathname } = request.nextUrl;
   const accessToken = cookies().get('accessToken')?.value || null;
 
-  if (pathname.startsWith(DOMAIN_URL.LOGIN) && accessToken) {
-    return NextResponse.redirect(new URL(DOMAIN_URL.MAIN, request.url));
+  if (pathname.startsWith('/login') && accessToken) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
-  if (pathname.startsWith(DOMAIN_URL.WEDDING_PRODUCE) && !accessToken) {
-    return NextResponse.redirect(new URL(DOMAIN_URL.LOGIN, request.url));
+  if (pathname.startsWith('/wedding/invitations/produce') && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  if (pathname.startsWith(DOMAIN_URL.PROFILE) && !accessToken) {
-    return NextResponse.redirect(new URL(DOMAIN_URL.LOGIN, request.url));
+  if (pathname.startsWith('/profile') && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 };
 
 export const config = {
-  matcher: [DOMAIN_URL.LOGIN, `${DOMAIN_URL.WEDDING_PRODUCE}/:id*`, DOMAIN_URL.PROFILE],
+  matcher: ['/login', '/wedding/invitations/produce/:id*', '/profile'],
 };
