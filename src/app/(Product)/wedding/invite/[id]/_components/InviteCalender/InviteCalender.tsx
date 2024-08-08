1d5df3910cb1b24f93bbb93bb6c058f005d8bebb
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-08 20:20:27 +0900

    🛠️refactor: 상수 분리

--- src/app/(Product)/wedding/invite/[id]/_components/InviteCalender/InviteCalender.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteCalender/InviteCalender.tsx
@@ -6,20 +6,11 @@ import { Calender } from '@/components/client';
 
 import { INVITE_ANIMATION } from '../../Invite.constants';
 import { INVITE_STYLE } from '../../Invite.style';
+import { CONVERT_DAY } from './InviteCalender.constants';
 import { InviteCalenderProps } from './InviteCalender.type';
 import { useConvertInviteDate } from './hooks';
 
 import { motion, useInView } from 'framer-motion';
-
-const CONVERT_DAY: { [key: number]: string } = {
-  0: '일요일',
-  1: '월요일',
-  2: '화요일',
-  3: '수요일',
-  4: '목요일',
-  5: '금요일',
-  6: '토요일',
-};
 
 const InviteCalender = ({ calenderData }: InviteCalenderProps) => {
   const date = useConvertInviteDate({ calenderData });
