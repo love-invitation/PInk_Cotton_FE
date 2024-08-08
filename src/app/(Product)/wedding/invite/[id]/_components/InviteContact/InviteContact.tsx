f3bf9376b41a7eb53b1f3a47d2ebac7d5a1be776
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-08 20:14:45 +0900

    🌱feat: InviteContact 타이틀 추가

--- src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
@@ -1,18 +1,23 @@
+import { INVITE_STYLE } from '../../Invite.style';
 import { InviteContactProps } from './InviteContact.type';
 import { ContactList } from './components';
 
 const InviteContact = ({ contactData }: InviteContactProps) => {
   return (
-    <article className='w-full py-[12rem] flex justify-around bg-[#f9f9f9] shadow-shadow_500 border-t-[0.1rem] border-b-[0.1rem]'>
-      <ContactList
-        type='groom'
-        data={contactData.groom}
-      />
-      <ContactList
-        type='bride'
-        data={contactData.bride}
-      />
-    </article>
+    <div className='w-full py-[8rem] flex flex-col gap-[4rem] bg-[#f9f9f9] shadow-shadow_500 border-t-[0.1rem] border-b-[0.1rem]'>
+      <h2 className={INVITE_STYLE.TITLE}>CONTACT</h2>
+
+      <article className='w-full flex justify-around '>
+        <ContactList
+          type='groom'
+          data={contactData.groom}
+        />
+        <ContactList
+          type='bride'
+          data={contactData.bride}
+        />
+      </article>
+    </div>
   );
 };
 
