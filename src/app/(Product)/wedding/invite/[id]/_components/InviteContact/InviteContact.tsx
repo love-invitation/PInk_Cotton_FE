ebdd199f6f0f98ca8759c310ca8a48edca841c09
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-10 14:01:57 +0900

    🛠️refactor: Invite style Layout 통합

--- src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteContact/InviteContact.tsx
@@ -2,9 +2,16 @@ import { INVITE_STYLE } from '../../Invite.style';
 import { InviteContactProps } from './InviteContact.type';
 import { ContactList } from './components';
 
+import { twJoin } from 'tailwind-merge';
+
 const InviteContact = ({ contactData }: InviteContactProps) => {
   return (
-    <div className='w-full py-[8rem] flex flex-col gap-[4rem] bg-[#f9f9f9] shadow-shadow_500 border-t-[0.1rem] border-b-[0.1rem]'>
+    <div
+      className={twJoin(
+        INVITE_STYLE.LAYOUT,
+        'gap-[4rem] bg-[#f9f9f9] shadow-shadow_500 border-t-[0.1rem] border-b-[0.1rem]',
+      )}
+    >
       <h2 className={INVITE_STYLE.TITLE}>CONTACT</h2>
 
       <article className='w-full flex justify-around '>
