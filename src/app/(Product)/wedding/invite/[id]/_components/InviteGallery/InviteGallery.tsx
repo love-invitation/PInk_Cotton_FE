aeba371e8e8a712d7149b2d5f804673916ff4ac5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-11 14:09:08 +0900

    🌱feat: Gallery layout title

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
@@ -1,5 +1,13 @@
+import { INVITE_STYLE } from '../../Invite.style';
+
+import { twMerge } from 'tailwind-merge';
+
 const InviteGallery = () => {
-  return <div>InviteGallery</div>;
+  return (
+    <article className={twMerge(INVITE_STYLE.LAYOUT, 'border-2 border-red-500')}>
+      <h2 className={INVITE_STYLE.TITLE}>GALLERY</h2>
+    </article>
+  );
 };
 
 export default InviteGallery;
