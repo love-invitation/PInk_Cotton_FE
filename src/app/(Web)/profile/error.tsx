c8b02d87f75bff87f79cd27e645622e75fdfdf44
wldlsgur <dhgg321@naver.com>
2024-08-14 23:23:21 +0900

    🎨style: 마이 페이지 에러 스타일 변경

--- src/app/(Web)/profile/error.tsx
+++ src/app/(Web)/profile/error.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/client';
 
 const Error = ({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary: () => void }) => {
   return (
-    <div className='flex flex-col justify-center items-center gap-[5rem]'>
+    <div className='flex flex-col justify-center items-center gap-[5rem] py-[9.9rem]'>
       <h2 className='text-[5rem]'>{error.toString()}</h2>
       <Button onClick={() => resetErrorBoundary()}>Try again</Button>
     </div>
