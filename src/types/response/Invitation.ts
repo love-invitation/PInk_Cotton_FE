916a337466fa4e35e0065a099bca3e388035a5ce
wldlsgur <dhgg321@naver.com>
2024-08-17 15:54:15 +0900

    🛠️refactor: 청첩장 단일 조회 반환 타입 수정

--- src/types/response/Invitation.ts
+++ src/types/response/Invitation.ts
@@ -1,157 +1,107 @@
-/** Cover */
-
-export interface InvitationCover {
-  priority: number;
-  templateId: number;
-  imageUrl: string;
-  imageOriginName: string;
-  imageStoreFileName: string;
-  weddingDate: string;
-  detail: string;
-  groomName: string;
-  brideName: string;
-  coverContents: string;
-}
-
-/** WeddingPlace */
-
-export interface InvitationWeddingPlace {
-  priority: number;
-  placeName: string;
-  detail: string;
-  placeAddress: string;
-  latitude: number;
-  longitude: number;
-}
-
-/** Article */
-
-export interface InvitationArticleItem {
-  name: string;
-  father: { name: string; deceased: boolean };
-  mother: { name: string; deceased: boolean };
-  relation: string;
-}
-
-export interface InvitationArticle {
-  priority: number;
-  title: string;
-  contents: string;
-  groom: InvitationArticleItem;
-  bride: InvitationArticleItem;
-}
-
-/** Contact */
-
-export interface InvitationContactItem {
-  name: string;
-  phoneNumber: string;
-  relation: string;
-}
-
-export interface InvitationContact {
-  priority: number;
-  groom: InvitationContactItem[];
-  bride: InvitationContactItem[];
-}
-
-/** Transport */
-
-export interface InvitationTransportItem {
-  kind: string;
-  detail: string;
-}
-
-export interface InvitationTransport {
-  priority: number;
-  transport: InvitationTransportItem[];
-}
-
-/** Account */
-
-export interface InvitationAccountItem {
-  name: string;
-  bankName: string;
-  accountNumber: string;
-}
-
-export interface InvitationAccount {
-  priority: number;
-  groomAccountInfo: InvitationAccountItem[];
-  brideAccountInfo: InvitationAccountItem[];
-}
-
-/** Gallery */
-
-export interface InvitationGalleryItem {
-  priority: number;
-  originFileName: string;
-  imageUrl: string;
-}
-
-export interface InvitationGallery {
-  priority: number;
-  galleries: InvitationGalleryItem[];
-}
-
 export interface InvitationResponse {
   status: number;
   message: string;
   result: {
-    tsid: bigint;
-    isPaid: boolean;
-
-    cover: InvitationCover;
-    place: InvitationWeddingPlace;
-
-    article: InvitationArticle;
-
-    contact: InvitationContact;
-    transport: InvitationTransport;
-
-    // guestbook: {
-    //   priority: number;
-    //   guestbookList: {
-    //     content: [
-    //       {
-    //         name: string;
-    //         message: string;
-    //       },
-    //     ];
-    //     pageable: {
-    //       pageNumber: number;
-    //       pageSize: number;
-    //       sort: {
-    //         empty: boolean;
-    //         unsorted: boolean;
-    //         sorted: boolean;
-    //       };
-    //       offset: number;
-    //       paged: boolean;
-    //       unpaged: boolean;
-    //     };
-    //     totalPages: number;
-    //     totalElements: number;
-    //     last: boolean;
-    //     size: number;
-    //     number: number;
-    //     sort: {
-    //       empty: boolean;
-    //       unsorted: boolean;
-    //       sorted: boolean;
-    //     };
-    //     numberOfElements: number;
-    //     first: boolean;
-    //     empty: boolean;
-    //   };
-    // };
-
-    account: InvitationAccount;
-    gallery: InvitationGallery;
-
-    shareThumbnail: {
-      shareTitle: string;
-      shareContents: string;
+    tsid: number;
+    guestbookCheck: boolean;
+    cover: {
+      priority: number;
+      templateId: number;
+      imageUrl: string;
+      imageOriginName: string;
+      imageStoreFileName: string;
+      weddingDate: string;
+      detail: string;
+      groomName: string;
+      brideName: string;
+      coverContents: string;
+    };
+    place: {
+      priority: number;
+      name: string;
+      detail: string;
+      address: string;
+      longitude: number;
+      latitude: number;
+    };
+    booking: {
+      priority: number;
+      date: string;
+      dateType?: string | null;
+    };
+    article: {
+      priority: number;
+      title: string;
+      contents: string;
+      groom: {
+        name: string;
+        father: {
+          name: string;
+          deceased: boolean;
+        };
+        mother: {
+          name: string;
+          deceased: boolean;
+        };
+        relation: string;
+      };
+      bride: {
+        name: string;
+        father: {
+          name: string;
+          deceased: boolean;
+        };
+        mother: {
+          name: string;
+          deceased: boolean;
+        };
+        relation: string;
+      };
+    };
+    contact: {
+      priority: number;
+      groom: {
+        name: string;
+        phoneNumber: string;
+        relation: string;
+      }[];
+      bride: {
+        name: string;
+        phoneNumber: string;
+        relation: string;
+      }[];
+    };
+    transport: {
+      priority: number;
+      transport: {
+        kind: string;
+        detail: string;
+      }[];
+    };
+    account: {
+      priority: number;
+      groom: {
+        name: string;
+        bankName: string;
+        accountNumber: string;
+      }[];
+      bride: {
+        name: string;
+        bankName: string;
+        accountNumber: string;
+      }[];
+    };
+    gallery: {
+      priority: number;
+      galleries: {
+        priority: number;
+        originFileName: string;
+        imageUrl: string;
+      }[];
+    };
+    thumbnail: {
+      title: string;
+      contents: string;
       imageUrl: string;
     };
   };
