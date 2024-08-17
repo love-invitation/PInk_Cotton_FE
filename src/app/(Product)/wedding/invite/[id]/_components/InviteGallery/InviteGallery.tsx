d7107b56689ada50d5d417fcd82ba4454c2c862d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-17 17:06:51 +0900

    🌱feat: GalleryItem 분리

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
@@ -2,13 +2,11 @@
 
 import { useCallback, useState } from 'react';
 
-import Image from 'next/image';
-
 import { useModal } from '@/hooks';
 
 import { INVITE_STYLE } from '../../Invite.style';
 import { InviteGalleryProps } from './InviteGallery.type';
-import { GalleryModal } from './components';
+import { GalleryItem, GalleryModal } from './components';
 
 import { twMerge } from 'tailwind-merge';
 
@@ -25,30 +23,16 @@ const InviteGallery = ({ galleryData }: InviteGalleryProps) => {
   );
 
   return (
-    <article className={twMerge(INVITE_STYLE.LAYOUT, 'gap-[6rem] border-2 border-red-500')}>
+    <article className={twMerge(INVITE_STYLE.LAYOUT, 'gap-[6rem]')}>
       <h2 className={INVITE_STYLE.TITLE}>GALLERY</h2>
 
       <ul className='w-full px-[1.6rem] flex flex-wrap gap-[1rem] items-center justify-center'>
         {galleryData.galleries.map(({ imageUrl, originFileName }, index) => (
-          <li
-            key={originFileName}
-            className='relative w-[12rem] h-[12rem] border-2 border-red-500'
-          >
-            <button
-              type='button'
-              className='w-full h-full relative'
-              onClick={() => handleClick(index)}
-            >
-              <Image
-                src={imageUrl}
-                fill
-                className='object-contain'
-                sizes='100%, 100%'
-                priority
-                alt={`gallery_image_${originFileName}`}
-              />
-            </button>
-          </li>
+          <GalleryItem
+            key={`galleryItem_${originFileName}`}
+            imageUrl={imageUrl}
+            onClick={() => handleClick(index)}
+          />
         ))}
       </ul>
 
---
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/components/GalleryItem/GalleryItem.type.ts
@@ -0,0 +1,5 @@
+export interface GalleryItemProps {
+  imageUrl: string;
+
+  onClick: () => void;
+}
