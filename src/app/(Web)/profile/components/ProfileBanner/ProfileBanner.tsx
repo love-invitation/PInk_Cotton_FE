c6b40cbd1144f2ca0b20729a07316e8d009a3596
wldlsgur <dhgg321@naver.com>
2024-08-19 03:21:10 +0900

    🛠️refactor: 프로파일 페이지 배너 텍스트 수정

--- src/app/(Web)/profile/components/ProfileBanner/ProfileBanner.tsx
+++ src/app/(Web)/profile/components/ProfileBanner/ProfileBanner.tsx
@@ -6,7 +6,7 @@ const ProfileBanner = () => {
       <h3 className='text-[2.4rem] font-semibold'>안내사항</h3>
       <ol className='list-disc text-[1.8rem] mx-[2.5rem]'>
         <li>결제 후에는 자유롭게 모바일청첩장을 사용할 수 있습니다.</li>
-        <li>제작일과 관계없이 예식일 +30일 후 청첩장이 삭제됩니다.</li>
+        <li>제작일과 관계없이 예식일 +90일 후 청첩장이 삭제됩니다.</li>
       </ol>
     </Banner>
   );
