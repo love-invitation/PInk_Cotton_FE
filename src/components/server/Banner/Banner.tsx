825527d3c1b0b001fdca58a29c8cb4ec97378f3f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-19 17:51:04 +0900

    🛠️refactor: Banner Drag 방지

--- src/components/server/Banner/Banner.tsx
+++ src/components/server/Banner/Banner.tsx
@@ -3,7 +3,11 @@ import { BannerProps } from './Banner.type';
 import { twMerge } from 'tailwind-merge';
 
 const Banner = ({ children, className }: BannerProps) => {
-  return <section className={twMerge('relative', className)}>{children}</section>;
+  return (
+    <section className={twMerge('relative pointer-events-none select-none', className)}>
+      {children}
+    </section>
+  );
 };
 
 export default Banner;
