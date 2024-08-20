287d2db644e5a090910ddc2b3dc0ab6268a8a18a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-20 17:22:20 +0900

    🌱feat: LocationActions 내부 Style & Nav List 생성

--- src/app/(Product)/wedding/invite/[id]/_components/InviteLocation/components/LocationActions/LocationActions.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteLocation/components/LocationActions/LocationActions.tsx
@@ -1,7 +1,60 @@
+'use client';
+
+import { useMemo } from 'react';
+
+import { PretendardFont } from '@/assets/font';
+import { Button } from '@/components/client';
+import { KakaoMapLogoIcon, NaverMapLogoIcon, TMapLogoIcon } from '@/components/server';
+
 const LocationActions = () => {
+  const NAV_LIST = useMemo(
+    () => [
+      {
+        key: 'naver',
+        buttonName: '네이버지도',
+        logoIcon: <NaverMapLogoIcon size='2rem' />,
+        onClick: () => {},
+      },
+      {
+        key: 'TMap',
+        buttonName: '티맵',
+        logoIcon: <TMapLogoIcon size='2rem' />,
+        onClick: () => {},
+      },
+      {
+        key: 'kakao',
+        buttonName: '카카오내비',
+        logoIcon: <KakaoMapLogoIcon size='2rem' />,
+        onClick: () => {},
+      },
+    ],
+    [],
+  );
+
   return (
-    <div className='w-[90%] flex flex-col gap-[1rem] border-[0.1rem] border-red-500'>
+    <div className='w-[90%] flex flex-col gap-[1rem]'>
       <p className=' text-size12'>아래 버튼을 누르면 앱에서 길안내가 시작됩니다.</p>
+
+      <ul className='w-full flex gap-[1rem]'>
+        {NAV_LIST.map(({ key, buttonName, logoIcon, onClick }) => (
+          <li
+            key={key}
+            className='flex-grow-[1]'
+          >
+            <Button
+              backgroundColor='naked'
+              fontColor='black'
+              border='gray'
+              fontWeight='regular'
+              className={`${PretendardFont.className} h-[3.6rem] m-0 p-0 w-full flex items-center justify-center gap-[0.6rem] text-size12 text-nowrap font-medium`}
+              onClick={onClick}
+            >
+              {logoIcon}
+              {buttonName}
+            </Button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
