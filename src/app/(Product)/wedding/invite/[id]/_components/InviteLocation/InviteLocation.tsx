2e31c6513c9131f0007d9ebf778cac573cf72a1b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-20 17:19:16 +0900

    🌱feat: Location Address Title 추가

--- src/app/(Product)/wedding/invite/[id]/_components/InviteLocation/InviteLocation.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteLocation/InviteLocation.tsx
@@ -12,6 +12,11 @@ const InviteLocation = ({ placeData }: InviteLocationProps) => {
     <article className={twMerge(INVITE_STYLE.LAYOUT, 'gap-[4rem]')}>
       <h2 className={INVITE_STYLE.TITLE}>LOCATION</h2>
 
+      <span className='w-full flex flex-col justify-center items-center gap-[1.2rem]'>
+        <p className='text-size18 font-bold'>{[placeData.name, placeData.detail].join(', ')}</p>
+        <p className='text-size14'>{placeData.address}</p>
+      </span>
+
       <PinkMap
         center={{ lat: placeData.latitude, lng: placeData.longitude }}
         height='32rem'
