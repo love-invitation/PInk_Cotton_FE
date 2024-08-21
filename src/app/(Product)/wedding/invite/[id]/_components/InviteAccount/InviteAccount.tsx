3b6248dcee842a61574dceab48f21897df56a8da
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-21 18:30:28 +0900

    🌱feat: InviteAccount 컴포넌트 생성 및 적용

---
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteAccount/InviteAccount.tsx
@@ -0,0 +1,11 @@
+import { INVITE_STYLE } from '../../Invite.style';
+
+const InviteAccount = () => {
+  return (
+    <article className={INVITE_STYLE.LAYOUT}>
+      <h2 className={INVITE_STYLE.TITLE}>ACCOUNT</h2>
+    </article>
+  );
+};
+
+export default InviteAccount;
