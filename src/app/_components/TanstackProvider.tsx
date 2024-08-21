9d66d4deaf45f3522bbd47780643ac4b067788cf
wldlsgur <dhgg321@naver.com>
2024-08-21 19:21:38 +0900

    🌱feat: API 에러 전역 처리

--- src/app/_components/TanstackProvider.tsx
+++ src/app/_components/TanstackProvider.tsx
@@ -5,30 +5,47 @@ import { PropsWithChildren, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { DOMAIN_URL, QUERY_KEYS, getQueryClient } from '@/constants';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const TanstackProvider = ({ children }: PropsWithChildren) => {
   const route = useRouter();
   const [queryClient] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-          refetchOnWindowFocus: false,
-          staleTime: 60 * 1000,
-          retryOnMount: false,
-        },
-        mutations: {
-          onError(error: unknown) {
-            if (error instanceof Error && error.toString().endsWith('401')) {
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error) => {
+            const errorMessage = error.toString();
+
+            if (error instanceof Error && errorMessage.endsWith('401')) {
+              route.push(DOMAIN_URL.LOGIN);
+              getQueryClient().invalidateQueries({ queryKey: QUERY_KEYS.AUTH_USER });
+            }
+
+            if (error instanceof Error && errorMessage.endsWith('403')) {
+              route.push('/notfound');
+            }
+          },
+        }),
+        mutationCache: new MutationCache({
+          onError: (error) => {
+            const errorMessage = error.toString();
+
+            if (error instanceof Error && errorMessage.endsWith('401')) {
               route.push(DOMAIN_URL.LOGIN);
               getQueryClient().invalidateQueries({ queryKey: QUERY_KEYS.AUTH_USER });
             }
           },
+        }),
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+            staleTime: 60 * 1000,
+            retryOnMount: false,
+          },
         },
-      },
-    }),
+      }),
   );
 
   return (
