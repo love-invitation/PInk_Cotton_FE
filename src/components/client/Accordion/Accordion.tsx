80d38ea49c7f0b2047317dd7aea43db992c2663d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-21 19:13:53 +0900

    🌱feat: Account Accordion type 추가

--- src/components/client/Accordion/Accordion.tsx
+++ src/components/client/Accordion/Accordion.tsx
@@ -2,12 +2,15 @@
 
 import React from 'react';
 
-import { ArrowDownIcon, Divider } from '@/components/server';
+import tailwindConfig from '@/../tailwind.config';
+import { AccountFlowerIcon, ArrowDownIcon, Divider } from '@/components/server';
 import { useToggle } from '@/hooks';
 
 import CheckBox from '../CheckBox/CheckBox';
 import { AccordionProps } from './Accordion.type';
 import { useAccordionStyle, useGetElementHeight } from './hooks';
+
+import resolveConfig from 'tailwindcss/resolveConfig';
 
 const Accordion = ({
   children,
@@ -18,6 +21,8 @@ const Accordion = ({
   const { isToggle, handleToggle } = useToggle(defaultToggleValue);
   const { ref, height } = useGetElementHeight();
   const { layoutStyle, buttonStyle, buttonInnerStyle, contentStyle } = useAccordionStyle({ type });
+
+  const { theme } = resolveConfig(tailwindConfig);
 
   return (
     <article className={layoutStyle}>
@@ -35,12 +40,20 @@ const Accordion = ({
           )}
           {type === 'main' && <strong className='text-size18 leading-[2] font-semibold'>Q.</strong>}
 
+          {type === 'account' && (
+            <AccountFlowerIcon
+              size='3rem'
+              fill={theme.colors.brown_500}
+            />
+          )}
+
           {buttonTitle}
         </div>
 
         <ArrowDownIcon
           size='2rem'
           className={`transition-transform duration-500 ${isToggle && 'rotate-180'}`}
+          fill={type === 'account' ? theme.colors.brown_500 : 'black'}
         />
       </button>
 
--- src/components/client/Accordion/hooks/useAccordionStyle/useAccordionStyle.ts
+++ src/components/client/Accordion/hooks/useAccordionStyle/useAccordionStyle.ts
@@ -23,6 +23,8 @@ const useAccordionStyle = ({ type }: UseAccordionStyle) => {
         'flex w-full select-none items-center break-keep text-start',
         type === 'edit' && 'h-[6rem] px-[2.4rem] text-size15 font-semibold',
         type === 'main' && 'h-[8rem] text-size16 font-semibold',
+        type === 'account' &&
+          'h-[6rem] px-[2rem] rounded-radius6 bg-brown_100 text-brown_500 text-size14 font-bold',
       ),
     [type],
   );
@@ -33,13 +35,19 @@ const useAccordionStyle = ({ type }: UseAccordionStyle) => {
         'flex flex-1 items-center',
         type === 'main' && 'gap-1 pl-[1rem]',
         type === 'edit' && 'gap-2',
+        type === 'account' && 'justify-center gap-[0.6rem]',
       ),
     [type],
   );
 
   const contentStyle = useMemo(
     () =>
-      twJoin('w-full', type === 'edit' && 'px-[2.4rem] py-2', type === 'main' && 'px-[1rem] pb-2'),
+      twJoin(
+        'w-full',
+        type === 'edit' && 'px-[2.4rem] py-2',
+        type === 'main' && 'px-[1rem] pb-2',
+        type === 'account' && 'rounded-radius6 bg-white_100',
+      ),
     [type],
   );
 
