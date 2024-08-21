da91e015a37923859b29b8f0fc1500dd8d82a91b
wldlsgur <dhgg321@naver.com>
2024-08-21 19:21:16 +0900

    🌱feat: 404 페이지 개발

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,29 @@
+'use client';
+
+import Lottie from 'react-lottie-player';
+
+import Link from 'next/link';
+
+import { NotFoundLottie } from '@/assets/lottie';
+import { DOMAIN_URL } from '@/constants';
+
 const NotFound = () => {
-  return '404 에러';
+  return (
+    <div className='flex flex-col gap-[3rem] justify-center items-center h-full w-full'>
+      <Lottie
+        animationData={NotFoundLottie}
+        className='h-[22rem] w-[22rem]'
+        play
+        loop
+      />
+      <Link
+        href={DOMAIN_URL.MAIN}
+        className='rounded-radius4 text-size14 bg-[#FF3C00] px-[1.6rem] py-[1rem] text-white_100 font-bold'
+      >
+        PinkCotton 사이트 바로가기
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
---
+++ src/assets/lottie/index.ts
@@ -0,0 +1,3 @@
+export { default as NotFoundLottie } from './notFound.json';
+export { default as AlertLottie } from './Alert.json';
+export { default as ConfirmLottie } from './Confirm.json';
