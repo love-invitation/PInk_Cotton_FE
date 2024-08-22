5eff3baa2ef5c5649a9861763ab6ce41f6446a66
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-22 15:54:38 +0900

    🌱feat: InviteAccount 내부 텍스트 구현

--- src/app/(Product)/wedding/invite/[id]/_components/InviteAccount/InviteAccount.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteAccount/InviteAccount.tsx
@@ -37,41 +37,48 @@ const InviteAccount = ({ accountData }: InviteAccountProps) => {
     <article className={twMerge(INVITE_STYLE.LAYOUT, 'px-[1.6rem] gap-[4rem]')}>
       <h2 className={INVITE_STYLE.TITLE}>ACCOUNT</h2>
 
-      <Accordion
-        buttonTitle='신랑측'
-        type='account'
-      >
-        <ul className='w-full border-[0.1rem] border-gray_700 rounded-radius6'>
-          {groom.map((accountInfo, index) => (
-            <Fragment key={accountInfo.name + accountInfo.bankName}>
-              {index !== 0 && <Divider />}
+      <span className='w-full px-[1.6rem] flex flex-col gap-[2rem] items-center'>
+        <p className='text-size18 font-bold'>마음 전하실 곳</p>
+        <p className='text-size14'>축복의 의미로 축의금을 전달해보세요.</p>
+      </span>
 
-              <InviteAccountItem
-                onCopy={handleCopyAccount}
-                accountInfo={accountInfo}
-              />
-            </Fragment>
-          ))}
-        </ul>
-      </Accordion>
+      <div className='w-full flex flex-col gap-[2rem]'>
+        <Accordion
+          buttonTitle='신랑측'
+          type='account'
+        >
+          <ul className='w-full border-[0.1rem] border-gray_700 rounded-radius6'>
+            {groom.map((accountInfo, index) => (
+              <Fragment key={accountInfo.name + accountInfo.bankName}>
+                {index !== 0 && <Divider />}
 
-      <Accordion
-        buttonTitle='신부측'
-        type='account'
-      >
-        <ul className='w-full border-[0.1rem] border-gray_700 rounded-radius6'>
-          {bride.map((accountInfo, index) => (
-            <Fragment key={accountInfo.name + accountInfo.bankName}>
-              {index !== 0 && <Divider />}
+                <InviteAccountItem
+                  onCopy={handleCopyAccount}
+                  accountInfo={accountInfo}
+                />
+              </Fragment>
+            ))}
+          </ul>
+        </Accordion>
 
-              <InviteAccountItem
-                onCopy={handleCopyAccount}
-                accountInfo={accountInfo}
-              />
-            </Fragment>
-          ))}
-        </ul>
-      </Accordion>
+        <Accordion
+          buttonTitle='신부측'
+          type='account'
+        >
+          <ul className='w-full border-[0.1rem] border-gray_700 rounded-radius6'>
+            {bride.map((accountInfo, index) => (
+              <Fragment key={accountInfo.name + accountInfo.bankName}>
+                {index !== 0 && <Divider />}
+
+                <InviteAccountItem
+                  onCopy={handleCopyAccount}
+                  accountInfo={accountInfo}
+                />
+              </Fragment>
+            ))}
+          </ul>
+        </Accordion>
+      </div>
     </article>
   );
 };
