4d42a4d1a89c868110a40c664c616ee2259c9021
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-22 15:45:47 +0900

    🛠️refactor: Data 구조분해 할당

--- src/app/(Product)/wedding/invite/[id]/page.tsx
+++ src/app/(Product)/wedding/invite/[id]/page.tsx
@@ -32,22 +32,24 @@ const InvitePage = async ({ params }: { params: { id: string } }) => {
     return null;
   }
 
+  const { account, cover, article, contact, gallery, place, transport } = data.result;
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <section className='w-full h-full flex justify-center bg-gray_900'>
         <div
           className={`max-w-[45rem] w-full h-full overflow-scroll bg-white scrollbar-hide shadow-shadow_500 font-nanum ${NanumMyeongjo.className}`}
         >
-          <InviteCover coverData={data.result.cover} />
-          <InviteArticle article={data.result.article} />
-          <InviteContact contactData={data.result.contact} />
-          <InviteCalender calenderData={data.result.cover.weddingDate} />
-          <InviteGallery galleryData={data.result.gallery} />
+          <InviteCover coverData={cover} />
+          <InviteArticle article={article} />
+          <InviteContact contactData={contact} />
+          <InviteCalender calenderData={cover.weddingDate} />
+          <InviteGallery galleryData={gallery} />
           <InviteLocation
-            placeData={data.result.place}
-            transportData={data.result.transport}
+            placeData={place}
+            transportData={transport}
           />
-          <InviteAccount />
+          <InviteAccount accountData={account} />
         </div>
       </section>
     </HydrationBoundary>
