fea3949c18f0d8c86eff1444f4661842a54184c1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-30 15:55:32 +0900

    🌱feat: Comment fetch 연결 및 기본 스타일 구현

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/BookComments/BookComments.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/BookComments/BookComments.tsx
@@ -1,5 +1,41 @@
+'use client';
+
+import { useCallback } from 'react';
+
+import { QUERY_OPTIONS } from '@/constants';
+import { GuestBook } from '@/types/response';
+import { useQuery } from '@tanstack/react-query';
+
 export const BookComments = () => {
-  return <ul>BookComments</ul>;
+  const { data } = useQuery<GuestBook>(QUERY_OPTIONS.GET_GUEST_BOOKS({ inviteId: 'key' }));
+
+  const convertDate = useCallback((date: string) => {
+    const changedDate = new Date(date);
+
+    return `${changedDate.getFullYear()}.${changedDate.getMonth() + 1}.${changedDate.getDate()}`;
+  }, []);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <ul className='w-full px-[1.6rem] flex flex-col gap-[2rem]'>
+      {data.result.content.map(({ message, created, name }) => (
+        <li
+          key={message}
+          className='w-full p-[1.6rem] flex flex-col gap-[1.6rem] shadow-shadow_500 relative pr-[3.6rem]'
+        >
+          <span className='w-full flex items-center gap-[1.6rem]'>
+            <h6 className='text-size15 font-bold flex-grow-[1]'>{name}</h6>
+            <p className='text-size14'>{convertDate(created)}</p>
+          </span>
+
+          <p className='text-size14'>{message}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default BookComments;
