91283fb6964c3c5eaf291084e5f224ff24aacd8b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-31 18:23:07 +0900

    🌱feat: CreateBookComment 등록 로직 개선

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/CreateBookComment/CreateBookComment.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/CreateBookComment/CreateBookComment.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import { FormProvider, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
-import { Button, Input } from '@/components/client';
+import { AlertModal, Button, Input } from '@/components/client';
 import { MUTATE_OPTIONS } from '@/constants';
+import { useToggle } from '@/hooks';
 import { useMutation } from '@tanstack/react-query';
 
 import { CreateBookCommentProps } from './CreateBookComment.type';
 
 const CreateBookComment = ({ inviteId }: CreateBookCommentProps) => {
+  const { isToggle, handleSetFalse, handleSetTrue } = useToggle();
   const formMethod = useForm({
     defaultValues: {
       name: '',
@@ -18,6 +20,21 @@ const CreateBookComment = ({ inviteId }: CreateBookCommentProps) => {
   });
 
   const { mutate } = useMutation(MUTATE_OPTIONS.INVITATION_GUEST_BOOK_POST());
+  const handleSubmit: SubmitHandler<{ name: string; password: string; message: string }> = (
+    data,
+  ) => {
+    mutate(
+      { inviteId, data },
+      {
+        onSuccess: () => {
+          formMethod.setValue('name', '');
+          formMethod.setValue('password', '');
+          formMethod.setValue('message', '');
+          handleSetTrue();
+        },
+      },
+    );
+  };
 
   return (
     <FormProvider {...formMethod}>
@@ -25,28 +42,67 @@ const CreateBookComment = ({ inviteId }: CreateBookCommentProps) => {
         <Input.Input
           name='name'
           placeholder='이름'
+          registerOptions={{
+            required: '이름을 입력해주세요.',
+          }}
         />
+
+        {formMethod.formState.errors.name && (
+          <Input.ErrorMessage>{formMethod.formState.errors.name.message}</Input.ErrorMessage>
+        )}
 
         <Input.Input
           name='password'
           placeholder='비밀번호'
+          registerOptions={{
+            required: '비밀번호를 입력해주세요.',
+            maxLength: {
+              message: '4글자 이상 12글자 이하로 작성해주세요.',
+              value: 12,
+            },
+            minLength: {
+              message: '4글자 이상 12글자 이하로 작성해주세요.',
+              value: 4,
+            },
+            pattern: {
+              value: /^[a-zA-Z0-9]+$/,
+              message: '영어, 숫자만 사용 가능 합니다.',
+            },
+          }}
         />
+
+        {formMethod.formState.errors.password && (
+          <Input.ErrorMessage>{formMethod.formState.errors.password.message}</Input.ErrorMessage>
+        )}
 
         <Input.Input
           name='message'
           placeholder='축하 메세지'
+          registerOptions={{
+            required: '메세지 입력해주세요.',
+          }}
         />
+
+        {formMethod.formState.errors.message && (
+          <Input.ErrorMessage>{formMethod.formState.errors.message.message}</Input.ErrorMessage>
+        )}
 
         <Button
           type='submit'
           radius='0.8rem'
           backgroundColor='black'
           className='h-fit py-[1.6rem]'
-          onClick={formMethod.handleSubmit((data) => mutate({ inviteId, data }))}
+          onClick={formMethod.handleSubmit(handleSubmit)}
         >
           축하메세지 남기기
         </Button>
       </div>
+
+      <AlertModal
+        isShow={isToggle}
+        onClose={handleSetFalse}
+        message='축하메세지가 등록되었습니다.'
+      />
     </FormProvider>
   );
 };
