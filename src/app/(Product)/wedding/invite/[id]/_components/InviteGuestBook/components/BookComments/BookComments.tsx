92c2d36c4e1e34a3a393bba6c353fe6a2eb1705a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-01 09:34:01 +0900

    🌱feat: Error 및 empty에 대한 처리

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/BookComments/BookComments.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGuestBook/components/BookComments/BookComments.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
-import { AlertModal, LoginModal, PasswordModal } from '@/components/client';
+import { AlertModal, Button, LoginModal, PasswordModal } from '@/components/client';
 import { QUERY_OPTIONS } from '@/constants';
 import { useToggle } from '@/hooks';
 import { GuestBook } from '@/types/response';
@@ -17,6 +17,7 @@ import {
   useCommentPages,
   useDeleteComment,
 } from './hooks';
+import { convertCommentDate } from './util';
 
 import { twJoin } from 'tailwind-merge';
 
@@ -36,7 +37,7 @@ export const BookComments = ({ inviteId }: BookCommentsProps) => {
     handleSetFalse: handleCloseLogin,
   } = useToggle();
 
-  const { data, refetch, isLoading } = useQuery<GuestBook>(
+  const { data, refetch, isLoading, isError } = useQuery<GuestBook>(
     QUERY_OPTIONS.GET_GUEST_BOOKS({ inviteId, page }),
   );
 
@@ -61,12 +62,6 @@ export const BookComments = ({ inviteId }: BookCommentsProps) => {
       handleOpenAlert();
     },
   });
-
-  const convertDate = useCallback((date: string) => {
-    const changedDate = new Date(date);
-
-    return `${changedDate.getFullYear()}.${changedDate.getMonth() + 1}.${changedDate.getDate()}`;
-  }, []);
 
   const handleNextPage = () => {
     setPage((prevPage) => {
@@ -118,12 +113,31 @@ export const BookComments = ({ inviteId }: BookCommentsProps) => {
       </div>
 
       <ul className='w-full min-h-[60rem] px-[1.6rem] flex flex-col gap-[2rem]'>
+        {isError && (
+          <li className='w-full py-[2rem] px-[10rem] flex justify-center items-center gap-[1.6rem] flex-col text-size14'>
+            <p>불러오는데 실패하였습니다.</p>
+
+            <Button
+              onClick={refetch}
+              backgroundColor='black'
+            >
+              재시도
+            </Button>
+          </li>
+        )}
+
+        {!isError && (!data || data.result.empty) && (
+          <li className='w-full py-[6rem] flex justify-center text-size14'>
+            <p>신랑 신부에게 축하메세지를 전해보세요!</p>
+          </li>
+        )}
+
         {data?.result.content.map(({ message, created, name, id }) => (
           <BookCommentItem
             key={created + name}
             message={message}
             name={name}
-            created={convertDate(created)}
+            created={convertCommentDate(created)}
             id={id}
             onDelete={handleChangeId}
           />
