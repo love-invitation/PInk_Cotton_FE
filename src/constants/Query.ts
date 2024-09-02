110414bbf25920a04fb2e8f36a59f5961d344c09
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-02 20:06:15 +0900

    📝change: GuestBook no store

--- src/constants/Query.ts
+++ src/constants/Query.ts
@@ -141,8 +141,8 @@ export const QUERY_OPTIONS = {
   GET_GUEST_BOOKS: ({ inviteId, page }: { inviteId: string; page: number }) => ({
     queryKey: QUERY_KEYS.GET_GUEST_BOOKS(inviteId, page),
     queryFn: () => getGuestBooks({ inviteId, page }),
-    gcTime: 1000 * 60 * 10,
-    staleTime: 1000 * 60 * 8,
+    gcTime: 0,
+    staleTime: 0,
   }),
 };
 
