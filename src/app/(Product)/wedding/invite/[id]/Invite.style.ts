2eca0dbb6a74612da1cd135fcd6a14593461ae05
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-02 14:02:23 +0900

    🎨style: Title Position 적용

--- src/app/(Product)/wedding/invite/[id]/Invite.style.ts
+++ src/app/(Product)/wedding/invite/[id]/Invite.style.ts
@@ -1,4 +1,5 @@
 export const INVITE_STYLE = {
-  TITLE: 'w-full flex justify-center text-size16 tracking-[0.4rem] text-center text-pink_500',
+  TITLE:
+    'w-full flex justify-center text-size16 tracking-[0.4rem] text-center text-pink_500 relative',
   LAYOUT: 'w-full h-fit py-[10rem] flex flex-col relative overflow-hidden select-none items-center',
 };
