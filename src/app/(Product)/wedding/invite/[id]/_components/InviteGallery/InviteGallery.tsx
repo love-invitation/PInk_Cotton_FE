466202a3e308fabe8cdb5cd5f67938cad0ef28cf
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-02 14:07:44 +0900

    🎨style: InviteGallery Animation 적용

--- src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
+++ src/app/(Product)/wedding/invite/[id]/_components/InviteGallery/InviteGallery.tsx
@@ -1,21 +1,32 @@
 'use client';
 
-import { useModal } from '@/hooks';
+import { useFramerInView, useModal } from '@/hooks';
 
+import { INVITE_ANIMATION } from '../../Invite.constants';
 import { INVITE_STYLE } from '../../Invite.style';
 import { InviteGalleryProps } from './InviteGallery.type';
 import { GalleryItem, GalleryModal } from './components';
 import { useGalleryRoute } from './hooks';
 
+import { motion } from 'framer-motion';
 import { twMerge } from 'tailwind-merge';
 
 const InviteGallery = ({ galleryData }: InviteGalleryProps) => {
   const { isShowModal, closeModal, showModal } = useModal();
   const { handleClick, viewNumber } = useGalleryRoute({ onActive: showModal });
+  const { ref, inView } = useFramerInView<HTMLHeadingElement>({ once: true });
 
   return (
     <article className={twMerge(INVITE_STYLE.LAYOUT, 'gap-[6rem]')}>
-      <h2 className={INVITE_STYLE.TITLE}>GALLERY</h2>
+      <motion.h2
+        ref={ref}
+        className={INVITE_STYLE.TITLE}
+        initial={INVITE_ANIMATION.INIT}
+        animate={inView ? INVITE_ANIMATION.ANIMATE : {}}
+        transition={INVITE_ANIMATION.DURATION}
+      >
+        GALLERY
+      </motion.h2>
 
       <ul className='w-full px-[1.6rem] flex flex-wrap gap-[1rem] items-center justify-center'>
         {galleryData.galleries.map(({ imageUrl, priority }, index) => (
@@ -23,6 +34,8 @@ const InviteGallery = ({ galleryData }: InviteGalleryProps) => {
             key={`galleryItem_${priority}`}
             imageUrl={imageUrl}
             onClick={() => handleClick(index)}
+            isInView={inView}
+            index={index}
           />
         ))}
       </ul>
