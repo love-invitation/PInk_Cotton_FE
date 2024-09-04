dfd6437e4f808bc17e202c840c7edb615a6f0b6c
wldlsgur <dhgg321@naver.com>
2024-09-04 19:57:02 +0900

    🌱feat: Weddings 메타 태그 추가

--- src/app/(Web)/wedding/invitations/layout.tsx
+++ src/app/(Web)/wedding/invitations/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from 'react';
 
+import { Metadata } from 'next';
+
+import { WEDDINGS_META_DATA } from '@/constants/MetaData';
+
 interface InvitationsLayoutProps {
   children: ReactNode;
   floating: ReactNode;
 }
+
+export const metadata: Metadata = WEDDINGS_META_DATA;
 
 const InvitationsLayout = ({ children, floating }: Readonly<InvitationsLayoutProps>) => {
   return (
