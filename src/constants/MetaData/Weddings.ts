c09017b61c9b61729a555d0b05bdd8b1468ce234
wldlsgur <dhgg321@naver.com>
2024-09-04 19:57:02 +0900

    🌱feat: Weddings 메타 태그 추가

---
+++ src/constants/MetaData/Weddings.ts
@@ -0,0 +1,24 @@
+const WEDDINGS_META_DATA = {
+  title: '청첩장 종류',
+  description: `다양한 디자인과 스타일로 나만의 청첩장을 선택해보세요. 전통적인 디자인부터 현대적인 스타일까지, 모든 취향을 만족시킬 다양한 청첩장을 제공합니다.`,
+  openGraph: {
+    title: '청첩장 종류',
+    description: `다양한 디자인과 스타일로 나만의 청첩장을 선택해보세요. 전통적인 디자인부터 현대적인 스타일까지, 모든 취향을 만족시킬 다양한 청첩장을 제공합니다.`,
+    url: `https://pinkcotton.shop/wedding/invitations`,
+    images: [
+      {
+        url: '/images/weddings-meta.png',
+        width: 800,
+        height: 600,
+        alt: '다양한 청첩장 디자인 이미지',
+      },
+    ],
+  },
+  twitter: {
+    title: '청첩장 종류',
+    description: `다양한 디자인과 스타일로 나만의 청첩장을 선택해보세요. 전통적인 디자인부터 현대적인 스타일까지, 모든 취향을 만족시킬 다양한 청첩장을 제공합니다.`,
+    images: ['/images/weddings-meta.png'],
+  },
+};
+
+export default WEDDINGS_META_DATA;
