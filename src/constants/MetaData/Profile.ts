488b62d66692efc64d4ad772a6d38c65ce30a141
wldlsgur <dhgg321@naver.com>
2024-09-04 19:55:57 +0900

    🌱feat: Profile MetaData 추가

---
+++ src/constants/MetaData/Profile.ts
@@ -0,0 +1,27 @@
+const PROFILE_META_DATA = {
+  title: '마이 페이지',
+  description:
+    '나만의 청첩장을 수정하고 미리보기를 확인하세요. 결제 및 공유 기능을 통해 특별한 날을 더욱 쉽게 준비할 수 있습니다.',
+  openGraph: {
+    title: '마이 페이지',
+    description:
+      '나만의 청첩장을 수정하고 미리보기를 확인하세요. 결제 및 공유 기능을 통해 특별한 날을 더욱 쉽게 준비할 수 있습니다.',
+    url: 'https://pinkcotton.shop/wedding/profile',
+    images: [
+      {
+        url: '/images/weddings-meta.png',
+        width: 800,
+        height: 600,
+        alt: '마이 페이지 이미지',
+      },
+    ],
+  },
+  twitter: {
+    title: '마이 페이지',
+    description:
+      '나만의 청첩장을 수정하고 미리보기를 확인하세요. 결제 및 공유 기능을 통해 특별한 날을 더욱 쉽게 준비할 수 있습니다.',
+    images: ['/images/weddings-meta.png'],
+  },
+};
+
+export default PROFILE_META_DATA;
